5c18bff6dfbbdc72c5304f571e1e559624c88254
Duncan Mortimer <dmortime@thoughtworks.com>
2015-01-08 12:52:17 +0000

    Add midje plugin to dev profile

--- project.clj
+++ project.clj
@@ -1,5 +1,6 @@
 (defproject d-cent "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
   :dependencies [[org.clojure/clojure "1.4.0"]]
-  :profiles {:dev {:dependencies [[midje "1.5.1"]]}})
+  :profiles {:dev {:dependencies [[midje "1.5.1"]]
+                   :plugins [[lein-midje "3.1.3"]]}})
   
