6a33b7e8cf0b16f3ff04d758805290fd32eb853a
Duncan Mortimer <dmortime@thoughtworks.com>
2015-01-08 12:35:09 +0000

    Create new project using Midje template

---
+++ src/d_cent/core.clj
@@ -0,0 +1,9 @@
+(ns d-cent.core)
+
+;;; This is an incorrect implementation, such as might be written by
+;;; someone who was used to a Lisp in which an empty list is equal to
+;;; nil.
+(defn first-element [sequence default]
+  (if (nil? sequence)
+    default
+    (first sequence)))
