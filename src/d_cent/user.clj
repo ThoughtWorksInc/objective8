17ae809355949f7566535525bf47066bbda2d383
Duncan Mortimer <dmortime@thoughtworks.com>
2015-01-27 18:35:08 +0000

    DM + CM #3 - Add function to user namespace for storing user's email address

--- src/d_cent/user.clj
+++ src/d_cent/user.clj
@@ -1,14 +1,8 @@
 (ns d-cent.user)
 
 (defn find-email-address-for-user [store user-id]
-  (get-in store [user-id :email-address])) 
+  (get-in @store [user-id :email-address])) 
 
-
-
-
-
-
-
-
-
-
+(defn store-email-address-for-user! [store user-id email-address]
+  (swap! store assoc-in [user-id :email-address] email-address)
+  store)
--- test/d_cent/user_test.clj
+++ test/d_cent/user_test.clj
@@ -5,26 +5,23 @@
 (def email-address "test@email.com")
 (def user-id :twitter-1)
 
+(facts "retrieve email"
+       (fact "can retrieve email address from store by user-id"
+             (find-email-address-for-user (atom {user-id {:email-address email-address}}) user-id) => email-address)
 
-(fact "can retrieve email address from store by user-id"
-      (find-email-address-for-user {user-id {:email-address email-address}} user-id) => email-address)
+       (fact "returns nil if user does not exist in store"
+             (find-email-address-for-user (atom {}) user-id) => nil)
 
-(fact "returns nil if user does not exist in store"
-      (find-email-address-for-user {} user-id) => nil)
-
-(fact "returns nil if user does not have email address in store"
-      (find-email-address-for-user {user-id {}} user-id) => nil)
-
-
+       (fact "returns nil if user does not have email address in store"
+             (find-email-address-for-user (atom {user-id {}}) user-id) => nil))
 
 
- 
+(defn stored-email-address-matches [user-id email-address]
+  (fn [store]
+    (= (find-email-address-for-user store user-id)
+       email-address)))
 
-
-
-
-
-
-
-
-
+(facts "store email"
+       (fact "can store an email address for a user"
+             (store-email-address-for-user! (atom {}) :the-user-id :the-email-address)
+             => (stored-email-address-matches :the-user-id :the-email-address)))
