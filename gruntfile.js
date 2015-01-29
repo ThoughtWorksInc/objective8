b96fd5b1a0cd60a866b0df942e32fbd045a61126
George Gray <ggray@thoughtworks.com>
2015-01-29 08:26:26 +0000

    GG - add autoprefixer to grunt tasks

--- gruntfile.js
+++ gruntfile.js
@@ -1,14 +1,23 @@
+'use strict';
+
 module.exports = function(grunt) {
+
+  require('load-grunt-tasks')(grunt);
+
   grunt.initConfig({    
     pkg: grunt.file.readJSON('package.json'),
     
     watch: {
+      gruntfile: {
+        files: ['gruntfile.js']
+      },
       sass: {
         files: ['resources/src/sass/**/*.scss'],
-        tasks: ['sass:dist']
+        tasks: ['sass:dist', 'autoprefixer']
       }
     },
     
+    //Compile SCSS
     sass: {
       options: {
         sourceMap: true,
@@ -19,20 +28,30 @@ module.exports = function(grunt) {
           'resources/public/styles.css': 'resources/src/sass/*.scss'
         }
       }
-    }
+    },
+    //Add vendor prefixed styles
+    autoprefixer: {
+      options: {
+        browsers: ['last 2 version', 'ie 8', 'ie 9']
+      },
+      no_dest: {
+        src: 'resources/public/styles.css'
+      },
+    },
   });
-
-  grunt.loadNpmTasks('grunt-sass');
-  grunt.loadNpmTasks('grunt-contrib-watch');  
-  
-  grunt.registerTask('build',[
-    'sass:dist'
-  ]);
 
   grunt.registerTask('dev',[
     'sass:dist',
+    'autoprefixer',
     'watch'
   ]);
+  
+  grunt.registerTask('build',[
+    'sass:dist',
+    'autoprefixer'
+  ]);
 
-  grunt.registerTask('default', 'build');
+  grunt.registerTask('default', [
+    'build'
+  ]);
 };
