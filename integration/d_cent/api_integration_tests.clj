19953ad659ce876d1ed60ff514047ec43a4e8770
Duncan Mortimer <dmortime@thoughtworks.com>
2015-01-29 17:27:56 +0000

    DM + GG #2 - Fix integration tests

--- integration/d_cent/api_integration_tests.clj
+++ integration/d_cent/api_integration_tests.clj
@@ -4,24 +4,45 @@
             [oauth.client :as oauth]
             [cheshire.core :as json]
             [d-cent.core :as core]
-            [d-cent.storage :as storage]
+            [d-cent.utils :as utils]
+            [d-cent.storage :as s]
             [d-cent.user :as user]))
 
 (def the-user-id "twitter-user_id")
 (def the-email-address "test@email.address.com")
 
+(def temp-store (atom {}))
+(def app-session (p/session (core/app (assoc core/app-config :store temp-store))))
+
+(def the-objective {:title "my objective title"
+                    :goals "my objective goals"
+                    :description "my objective description"
+                    :end-date "my objective end-date"
+                    :created-by "some dude"})
+
+(fact "Objectives posted to the API get stored"
+      (let [request-to-create-objective (p/request app-session "/api/v1/objectives"
+                                                   :request-method :post
+                                                   :content-type "application/json"
+                                                   :body (json/generate-string the-objective))
+            response (:response request-to-create-objective)
+            headers (:headers response)]
+        response => (contains {:status 201})
+        headers => (contains {"Location" "value"})
+        (s/find-by temp-store "objectives" (constantly true)) => (contains the-objective)))
+
 (fact "should be able to store email addresses"
-      (let [store (atom {})
-            app-config (into core/app-config {:store store})
-            app (core/app app-config)]
+      (let [temp-store (atom {})
+            app-config (into core/app-config {:store temp-store})
+            user-session (p/session (core/app app-config))]
         (do
-          (-> (p/session app)
+          (-> user-session
               (p/content-type "application/json")
               (p/request "/api/v1/users"
                          :request-method :post
                          :headers {"Content-Type" "application/json"}
                          :body (json/generate-string {:email-address the-email-address
                                                       :user-id the-user-id})))
-          (:email-address (user/retrieve-user-record store the-user-id))))
+          (:email-address (user/retrieve-user-record temp-store the-user-id))))
       => the-email-address)
 
