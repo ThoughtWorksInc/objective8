5e4becf85c795b6aef253a2edf92a86f3ed4f47c
Gareth <grogers@thoughtworks.com>
2015-02-04 18:07:45 +0000

    GR - created initial database mapping for objectives

--- src/d_cent/storage.clj
+++ src/d_cent/storage.clj
@@ -1,4 +1,40 @@
-(ns d-cent.storage)
+(ns d-cent.storage
+  (:require [korma.core :as korma]
+            [korma.db :as db]
+            [cheshire.core :as json])
+  (:import [org.postgresql.util PGobject]))
+
+(def dbspec (db/postgres {:db "dcent"
+                          :user "dcent"
+                          :password "development"
+                          :host "localhost"}))
+
+(defn map->json-type
+  "Convert a clojure map to a Postgres JSON type"
+  [m]
+  (doto (PGobject.)
+    (.setType "json")
+    (.setValue (json/generate-string m)))) 
+
+(defn connect! []
+  (db/default-connection (db/create-db dbspec))) 
+
+(korma/defentity objective
+  (korma/pk :_id)
+  (korma/table :policy_drafting.objectives)
+  (korma/prepare (fn [{:keys [created-by] :as objective}]
+                   {:created_by created-by
+                    :objective (map->json-type objective)})))
+
+(def database-mappings {:objective objective})
+
+(defn stuff []
+  (korma/insert objective (korma/values {:created-by "foo" :something "bar"}))) 
+
+(defn pg-store! [{:keys [entity] :as m}]
+  (if-let [ent (get database-mappings entity)]
+    (korma/insert ent (korma/values (dissoc m :entity)))
+    (throw (Exception. "Could not find database mapping for " entity))))
 
 (defn request->store
   "Fetches the storage atom from the request"
