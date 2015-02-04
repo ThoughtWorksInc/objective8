0f31cc35f6df6eb25580850cc79fe36065fb9d7a
George Gray <ggray@thoughtworks.com>
2015-02-04 12:23:48 +0000

    DM/GG #4 basic share icons on view objective working

--- src/d_cent/responses.clj
+++ src/d_cent/responses.clj
@@ -1,12 +1,15 @@
 (ns d-cent.responses
   (:require [net.cgrand.enlive-html :as html]
             [d-cent.translation :refer [translation-config]]
-            [d-cent.config :as config]))
+            [d-cent.config :as config]
+            [d-cent.utils :as utils]))
 
 (def google-analytics-tracking-id (config/get-var "GA_TRACKING_ID"))
+(defn objective-url [objective]
+  (str utils/host-url "/objectives/" (:_id objective)))
 
 ;GOOGLE ANALYTICS
-(html/defsnippet google-analytics 
+(html/defsnippet google-analytics
   "templates/google-analytics.html" [[:#clj-google-analytics]] [tracking-id]
   (html/transform-content (html/replace-vars {:trackingID tracking-id})))
 
@@ -39,20 +42,20 @@
                   [:body] (html/append (if google-analytics-tracking-id (google-analytics google-analytics-tracking-id))))
 
 ;NAVIGATION
-(html/defsnippet global-navigation-signed-in 
+(html/defsnippet global-navigation-signed-in
   "templates/navigation-global-signed-in.html" [[:.global-navigation]] [{:keys [translation]}]
   [:#clj-sign-out-link] (html/content (translation :navigation-global/sign-out-text))
   [:#clj-sign-out-link-title] (html/set-attr :title (translation :navigation-global/sign-out-title))
   [:#clj-user-profile-link] (html/content (translation :navigation-global/profile-text))
   [:#clj-user-profile-link] (html/set-attr :title (translation :navigation-global/profile-title)))
 
-(html/defsnippet global-navigation-signed-out 
+(html/defsnippet global-navigation-signed-out
   "templates/navigation-global-signed-out.html" [[:.global-navigation]] [{:keys [translation]}]
   [:#clj-sign-in-link] (html/content (translation :navigation-global/sign-in-text))
   [:#clj-sign-in-link-title] (html/set-attr :title (translation :navigation-global/sign-in-title)))
 
 ;HOME/INDEX
-(html/defsnippet index-page 
+(html/defsnippet index-page
   "templates/index.html" [[:#clj-index]] [{:keys [translation]}]
   [:.index-welcome] (html/content (translation :index/index-welcome))
   [:.index-intro] (html/content (translation :index/index-intro))
@@ -62,7 +65,7 @@
   [:.index-learn-more] (html/set-attr :title (translation :index/index-learn-more-title)))
 
 ;SIGN IN
-(html/defsnippet sign-in-page 
+(html/defsnippet sign-in-page
   "templates/sign-in.html" [[:#clj-sign-in-page]] [{:keys [translation]}]
   [:h1] (html/content (translation :sign-in/page-title))
   [:#clj-sign-in-twitter] (html/set-attr :value (translation :sign-in/twitter-sign-in-btn))
@@ -70,7 +73,7 @@
 
 
 ;OBJECTIVES
-(html/defsnippet objective-create-page 
+(html/defsnippet objective-create-page
   "templates/objectives-create.html" [[:#clj-objective-create]] [{:keys [translation]}]
   [:h1] (html/content (translation :objective-create/page-title))
   [(html/attr= :for "objective-title")] (html/content (translation :objective-create/title-label))
@@ -83,18 +86,26 @@
   [(html/attr= :name "end-date")] (html/set-attr :title (translation :objective-create/end-date-title))
   [:.button] (html/content (translation :objective-create/submit)))
 
-(html/defsnippet objective-view-page "templates/objectives-view.html" [[:#clj-objectives-view]]
-                                                                       [{:keys [translation objective]}]
-                                                                       [:h1] (html/content (:title objective))
-                                                                       [:#clj-obj-goals-label] (html/content (translation :objective-view/goals-label))
-                                                                       [:#clj-obj-goals-value] (html/content (:goals objective))
-                                                                       [:#clj-obj-description-label] (html/content (translation :objective-view/description-label))
-                                                                       [:#clj-obj-description-value] (html/content (:description objective))
-                                                                       [:#clj-obj-date-label] (html/content (translation :objective-view/end-date-label))
-                                                                       [:#clj-obj-end-date-value] (html/content (:end-date objective)))
+(html/defsnippet objective-view-page
+  "templates/objectives-view.html" [[:#clj-objectives-view]]
+  [{:keys [translation objective]}]
+  [:h1] (html/content (:title objective))
+  [:#clj-obj-goals-label] (html/content (translation :objective-view/goals-label))
+  [:#clj-obj-goals-value] (html/content (:goals objective))
+  [:#clj-obj-description-label] (html/content (translation :objective-view/description-label))
+  [:#clj-obj-description-value] (html/content (:description objective))
+  [:#clj-obj-date-label] (html/content (translation :objective-view/end-date-label))
+  [:#clj-obj-end-date-value] (html/content (:end-date objective))
+  [:.share-widget html/any-node] (html/replace-vars translation)
+  [:.btn-facebook] (html/set-attr :href (str "http://www.facebook.com/sharer.php?u=" (objective-url objective) "t=" (:title objective) " - "))
+  [:.btn-google-plus] (html/set-attr :href (str "https://plusone.google.com/_/+1/confirm?hl=en&url=" (objective-url objective)))
+  [:.btn-twitter] (html/set-attr :href (str "https://twitter.com/share?url=" (objective-url objective) "&text=" (:title objective) " - "))
+  [:.btn-linkedin] (html/set-attr :href (str "http://www.linkedin.com/shareArticle?mini=true&url=" (objective-url objective)))
+  [:.btn-reddit] (html/set-attr :href (str "http://reddit.com/submit?url=" (objective-url objective) "&title=" (:title objective) " - "))
+  [:.share-this-url] (html/set-attr :value (objective-url objective)))
 
 ;USERS
-(html/defsnippet users-email 
+(html/defsnippet users-email
   "templates/users-email.html" [[:#clj-users-email]] [{:keys [translation]}]
   [:h1] (html/content (translation :users-email/page-title))
   [:.clj-user-email-welcome] (html/content (translation :users-email/user-email-welcome))
