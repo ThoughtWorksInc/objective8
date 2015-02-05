eceacba90c4b1bc056254b0245c27ffa1e5e5df5
Duncan Mortimer <dmortime@thoughtworks.com>
2015-02-05 17:20:53 +0000

    DM/RS/GR - #40 Integration tests for some-sql DB + entity transformations

--- src/d_cent/storage/mappings.clj
+++ src/d_cent/storage/mappings.clj
@@ -11,6 +11,11 @@
     (.setType "json")
     (.setValue (json/generate-string m))))
 
+(defn json-type->map
+  "Convert a Postgres JSON type to a clojure map"
+  [pgobject]
+  (json/parse-string (.getValue pgobject) true))
+
 (defn map->objective
   "Converts a clojure map into a json-typed objective for the database"
   [{:keys [created-by end-date] :as objective}]
@@ -28,15 +33,20 @@
      :user_data (map->json-type user)}
     (throw (Exception. "Could not transform map to user"))))
 
+(defn unmap [data-key]
+  (fn [m] (assoc (json-type->map (data-key m)) :_id (:_id m))))
+
 (korma/defentity objective
   (korma/pk :_id)
   (korma/table :policy_drafting.objectives)
-  (korma/prepare map->objective))
+  (korma/prepare map->objective)
+  (korma/transform (unmap :objective)))
 
 (korma/defentity user
   (korma/pk :_id)
   (korma/table :policy_drafting.users)
-  (korma/prepare map->user))
+  (korma/prepare map->user)
+  (korma/transform (unmap :user_data)))
 
 (def entities {:objective objective
                :user      user})
