7d24976a08d9af5d66e9deb2c25aa81aee9c2ada
Duncan Mortimer <dmortime@thoughtworks.com>
2015-02-06 12:39:44 +0000

    DM/RS - #40 Get snap ci DB config from environment

--- src/d_cent/storage/database.clj
+++ src/d_cent/storage/database.clj
@@ -1,10 +1,12 @@
 (ns d-cent.storage.database
-  (:require [korma.db :as db]))
+  (:require [korma.db :as db]
+            [d-cent.config :as config]))
 
 (def postgres-spec (db/postgres {:db "dcent"
-                                 :user "dcent"
-                                 :password "development" ;TODO password management
-                                 :host "localhost"}))
+                                 :user (config/get-var "SNAP_DB_PG_USER" "dcent")
+                                 :password (config/get-var "SNAP_DB_PG_PASSWORD" "development") ;TODO password management
+                                 :host (config/get-var "SNAP_DB_PG_HOST" "localhost")
+                                 :port (config/get-var "SNAP_DB_PG_PORT" 5432)}))
 
 (defn connect!
   "Connect to the database described by the DB spec"
