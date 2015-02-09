76499642840b55ffc545b5dcf51fe0761a191e8b
Gareth <grogers@thoughtworks.com>
2015-02-09 14:47:07 +0000

    GR - add bash script for setting up environment variables

---
+++ snap.env.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+export DB_USER=$SNAP_DB_PG_USER
+export DB_PASSWORD=$SNAP_DB_PG_PASSWORD
+export DB_HOST=$SNAP_DB_PG_HOST
+export DB_PORT=$SNAP_DB_PG_PORT
+export DB_NAME=$SNAP_DB_PG_NAME
+export DB_JDBC_URL=$SNAP_DB_PG_JDBC_URL
