7e8206b8cd0a7c11134e3adf4c2621398c8e5ebf
Rob Speller <rspeller@thoughtworks.com>
2015-02-10 14:44:26 +0000

    RS+CM+GR | Attempt to fix heroku environment variables

--- heroku.env.sh
+++ heroku.env.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
 HEROKU_POSTGRES_URL=$DATABASE_URL
+
 DB_URL=`echo $HEROKU_POSTGRES_URL | cut -d@ -f2`
 DB_CONNECTION=`echo $HEROKU_POSTGRES_URL | cut -d@ -f1`
 
 export BASE_URI="fast-lake-9564.herokuapp.com"
 export DB_USER=`echo $DB_CONNECTION | cut -d: -f2 | cut -c 3-`
 export DB_PASSWORD=`echo $DB_CONNECTION | cut -d: -f3`
-export DB_HOST=`echo $DB_URL | cut -d/ -f1`
+export DB_HOST=`echo $DB_URL | cut -d: -f1`
+export DB_PORT=`echo $DB_URL | cut -d/ -f1 | cut -d: -f2`
 export DB_NAME=`echo $DB_URL | cut -d/ -f2`
-export DB_JDBC_URL="jdbc:postgresql://${DB_HOST}/${DB_NAME}?user=${DB_USER}&password=${DB_PASSWORD}"
+export DB_JDBC_URL="jdbc:postgresql://${DB_HOST}:${DB_PORT}/${DB_NAME}?user=${DB_USER}&password=${DB_PASSWORD}"
 
