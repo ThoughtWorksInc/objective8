8875b5cac6f7f5065fc4cbea722ee63022d69b97
Gareth <grogers@thoughtworks.com>
2015-02-10 11:28:45 +0000

    GR - use postgres on heroku

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: lein run
+web: source heroku.env.sh && lein do ragtime migrate, trampoline run
---
+++ heroku.env.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+HEROKU_POSTGRES_URL=$DATABASE_URL
+DB_URL=`echo $HEROKU_POSTGRES_URL | cut -d@ -f2`
+DB_CONNECTION=`echo $HEROKU_POSTGRES_URL | cut -d@ -f1`
+
+export DB_USER=`echo $DB_CONNECTION | cut -d: -f2 | cut -c 3-`
+export DB_PASSWORD=`echo $DB_CONNECTION | cut -d: -f3`
+export DB_HOST=`echo $DB_URL | cut -d/ -f1`
+export DB_NAME=`echo $DB_URL | cut -d/ -f2`
+export DB_JDBC_URL="jdbc:postgresql://${DB_HOST}/${DB_NAME}?user=${DB_NAME}&password=${DB_PASSWORD}"
+
