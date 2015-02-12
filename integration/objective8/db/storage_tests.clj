4453ecaf4c331d876365bd92675f41b73fb5c987
George Gray <ggray@thoughtworks.com>
2015-02-12 13:34:57 +0000

    CM/GG #7 - fixing intergration tests, tear down inside tests

--- integration/objective8/db/storage_tests.clj
+++ integration/objective8/db/storage_tests.clj
@@ -14,82 +14,83 @@
   (korma/delete m/objective)
   (korma/delete m/user))
 
-(against-background
- [(before :contents (db-connection)) (after :facts (truncate-tables))]
- (facts "Storage tests" :integration
-        ;;USERS
-        (fact "a user entity can be stored in the database"
-              (let [user {:entity :user :twitter-id "the-twitter-id" :foo "bar"}
-                    store-result (storage/pg-store! user)
-                    retrieve-result (storage/pg-retrieve {:entity :user :_id (:_id store-result)})]
-                (first (:result retrieve-result))) => (contains {:twitter-id "the-twitter-id" :foo "bar"}))
+(facts "Storage tests" :integration
+  (against-background
+   [(before :contents (db-connection)) (after :facts (truncate-tables))]
+   (facts "About storage"
+          ;;USERS
+          (fact "a user entity can be stored in the database"
+                (let [user {:entity :user :twitter-id "the-twitter-id" :foo "bar"}
+                      store-result (storage/pg-store! user)
+                      retrieve-result (storage/pg-retrieve {:entity :user :_id (:_id store-result)})]
+                  (first (:result retrieve-result))) => (contains {:twitter-id "the-twitter-id" :foo "bar"}))
 
-        ;;OBJECTIVES
-        (fact "an objective entity can be stored in the database"
-              (let [stored-user (storage/pg-store! {:entity :user
-                                                    :twitter-id "twitter-TWITTER_ID"})
-                    user-id (:_id stored-user)
-                    objective {:entity :objective
+          ;;OBJECTIVES
+          (fact "an objective entity can be stored in the database"
+                (let [stored-user (storage/pg-store! {:entity :user
+                                                      :twitter-id "twitter-TWITTER_ID"})
+                      user-id (:_id stored-user)
+                      objective {:entity :objective
+                                 :created-by-id user-id
+                                 :end-date "2015-01-01T00:00:00Z"
+                                 :description "description"
+                                 :goals "goals"
+                                 :title "title"}
+                      store-result (storage/pg-store! objective)
+                      retrieve-result (storage/pg-retrieve {:entity :objective :_id (:_id store-result)})]
+                  (first (:result retrieve-result)) => (contains {:created-by-id user-id
+                                                                  :end-date "2015-01-01T00:00:00Z"
+                                                                  :title "title"})))
+          ;;COMMENTS
+          (fact "a comment entity can be stored in the database"
+                (let [stored-user (storage/pg-store! {:entity :user
+                                                      :twitter-id "twitter-TWITTER_ID"})
+                      user-id (:_id stored-user)
+                      stored-objective (storage/pg-store! {:entity :objective
+                                                           :created-by-id user-id
+                                                           :end-date "2015-01-01T00:00:00.000Z"})
+                      objective-id (:_id stored-objective)
+                      comment {:entity :comment
                                :created-by-id user-id
-                               :end-date "2015-01-01T00:00:00Z"
-                               :description "description"
-                               :goals "goals"
-                               :title "title"}
-                    store-result (storage/pg-store! objective)
-                    retrieve-result (storage/pg-retrieve {:entity :objective :_id (:_id store-result)})]
-                (first (:result retrieve-result)) => (contains {:created-by-id user-id
-                                                                :end-date "2015-01-01T00:00:00Z"
-                                                                :title "title"})))
-        ;;COMMENTS
-        (fact "a comment entity can be stored in the database"
-              (let [stored-user (storage/pg-store! {:entity :user
-                                                    :twitter-id "twitter-TWITTER_ID"})
-                    user-id (:_id stored-user)
-                    stored-objective (storage/pg-store! {:entity :objective
-                                                         :created-by-id user-id
-                                                         :end-date "2015-01-01T00:00:00.000Z"})
-                    objective-id (:_id stored-objective)
-                    comment {:entity :comment
-                             :created-by-id user-id
-                             :objective-id objective-id
-                             :comment "A comment"}
-                    store-result (storage/pg-store! comment)
-                    retrieve-result (storage/pg-retrieve {:entity :comment :_id (:_id store-result)})]
-                (first (:result retrieve-result)) => (contains {:created-by-id user-id
-                                                                :objective-id objective-id})))
+                               :objective-id objective-id
+                               :comment "A comment"}
+                      store-result (storage/pg-store! comment)
+                      retrieve-result (storage/pg-retrieve {:entity :comment :_id (:_id store-result)})]
+                  (first (:result retrieve-result)) => (contains {:created-by-id user-id
+                                                                  :objective-id objective-id})))
 
-        (fact "can limit the number of results when retrieving"
-              (let [users [{:entity :user :twitter-id "the-twitter-id1"}
-                           {:entity :user :twitter-id "the-twitter-id2"}
-                           {:entity :user :twitter-id "the-twitter-id3"}]
-                    store-result (doall (map storage/pg-store! users))
-                    retrieve-result (storage/pg-retrieve {:entity :user} {:limit 2})]
-                (count (:result retrieve-result)) => 2))
+          (fact "can limit the number of results when retrieving"
+                (let [users [{:entity :user :twitter-id "the-twitter-id1"}
+                             {:entity :user :twitter-id "the-twitter-id2"}
+                             {:entity :user :twitter-id "the-twitter-id3"}]
+                      store-result (doall (map storage/pg-store! users))
+                      retrieve-result (storage/pg-retrieve {:entity :user} {:limit 2})]
+                  (count (:result retrieve-result)) => 2))
 
-        (fact "results are retrieved in reverse chronological order"
-              (let [users [{:entity :user :twitter-id "the-twitter-id1"}
-                           {:entity :user :twitter-id "the-twitter-id2"}
-                           {:entity :user :twitter-id "the-twitter-id3"}]
-                    store-result (doall (map storage/pg-store! users))
-                    retrieved-users (:result (storage/pg-retrieve {:entity :user}))
-                    sorted-twitter-ids (vec (map :twitter-id retrieved-users))]
-                sorted-twitter-ids)
-              => ["the-twitter-id3" "the-twitter-id2" "the-twitter-id1"]))
+          (fact "results are retrieved in reverse chronological order"
+                (let [users [{:entity :user :twitter-id "the-twitter-id1"}
+                             {:entity :user :twitter-id "the-twitter-id2"}
+                             {:entity :user :twitter-id "the-twitter-id3"}]
+                      store-result (doall (map storage/pg-store! users))
+                      retrieved-users (:result (storage/pg-retrieve {:entity :user}))
+                      sorted-twitter-ids (vec (map :twitter-id retrieved-users))]
+                  sorted-twitter-ids)
+                => ["the-twitter-id3" "the-twitter-id2" "the-twitter-id1"]))
 
-        ;;QUESTIONS
-        (fact "a question entity can be stored in the database"
-              (let [stored-user (storage/pg-store! {:entity :user
-                                                    :twitter-id "twitter-TWITTER_ID"})
-                    user-id (:_id stored-user)
-                    stored-objective (storage/pg-store! {:entity :objective
-                                                         :created-by-id user-id
-                                                         :end-date "2015-01-01T00:00:00.000Z"})
-                    objective-id (:_id stored-objective)
-                    question {:entity :question
-                              :created-by-id user-id
-                              :objective-id objective-id
-                              :question "A question"}
-                    store-result (storage/pg-store! question)
-                    retrieve-result (storage/pg-retrieve {:entity :question :_id (:_id store-result)})]
-                (first (:result retrieve-result)) => (contains {:created-by-id user-id
-                                                                :objective-id objective-id}))))
+          ;;QUESTIONS
+          (fact "a question entity can be stored in the database"
+                (let [stored-user (storage/pg-store! {:entity :user
+                                                      :twitter-id "twitter-TWITTER_ID"})
+                      user-id (:_id stored-user)
+                      stored-objective (storage/pg-store! {:entity :objective
+                                                           :created-by-id user-id
+                                                           :end-date "2015-01-01T00:00:00.000Z"})
+                      objective-id (:_id stored-objective)
+                      question {:entity :question
+                                :created-by-id user-id
+                                :objective-id objective-id
+                                :question "A question"}
+                      store-result (storage/pg-store! question)
+                      retrieve-result (storage/pg-retrieve {:entity :question :_id (:_id store-result)})]
+                  (first (:result retrieve-result)) => (contains {:created-by-id user-id
+                                                                  :objective-id objective-id})))))
