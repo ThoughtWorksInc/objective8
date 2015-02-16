0ff9a56bfabe746ab2d3677e31fea206cca2348f
George Gray <ggray@thoughtworks.com>
2015-02-16 14:38:11 +0000

    DM/RS/GG - update response test to use objective-detail page

--- test/objective8/responses_test.clj
+++ test/objective8/responses_test.clj
@@ -5,24 +5,24 @@
             [objective8.utils :as utils]))
 
 (fact "'Sign-in to comment' link target on objective detail view refers back to current url"
-      (-> (responses/objective-view-page {:translation (constantly "---")
-                                          :objective {}
-                                          :signed-in false
-                                          :comments []
-                                          :uri "CURRENT_PAGE_URI"})
-          (html/select [:#clj-comment-sign-in :a])
+      (-> (responses/objective-detail-page {:translation (constantly "---")
+                                            :objective {}
+                                            :signed-in false
+                                            :comments []
+                                            :uri "CURRENT_PAGE_URI"})
+          (html/select [:#clj-comment-sign-in-uri])
           first)
       => (contains {:attrs (contains {:href (contains "refer=CURRENT_PAGE_URI")})}))
 
 (facts "Splitting text into paragraphs"
        (fact "Newlines are turned into paragraphs"
              (responses/text->p-nodes "Hello\nWorld") =>
-             [[{:tag :p :content '("Hello") :attrs {}}] 
+             [[{:tag :p :content '("Hello") :attrs {}}]
               [{:tag :p :content '("World") :attrs {}}]])
 
        (fact "Collapses multiple newlines into single paragraphs"
              (responses/text->p-nodes "Hello\n\n\nWorld\n\r\nFoo\n   \n \n \nBar Zap\r\t\n") =>
-             [[{:tag :p :content '("Hello") :attrs {}}] 
+             [[{:tag :p :content '("Hello") :attrs {}}]
               [{:tag :p :content '("World") :attrs {}}]
               [{:tag :p :content '("Foo") :attrs {}}]
               [{:tag :p :content '("Bar Zap") :attrs {}}]])
