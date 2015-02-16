2ee5c792fcaf84854bf6b12a3b7066ee6758b915
Rob Speller <rspeller@thoughtworks.com>
2015-02-16 17:15:59 +0000

    RS + DM #8 - Include Location in response headers after posting an answer

--- integration/objective8/api/answers_integration_tests.clj
+++ integration/objective8/api/answers_integration_tests.clj
@@ -31,5 +31,18 @@
 
              => (helpers/check-json-body stored-answer)
              (provided
-               (answers/store-answer! the-answer) => stored-answer)))
+               (answers/store-answer! the-answer) => stored-answer)
 
+      (fact "the http response indicates the location of the answer"
+            (against-background
+              (answers/store-answer! anything) => stored-answer)
+            (let [result (p/request app (str "/api/v1/objectives/" OBJECTIVE_ID "/questions/" QUESTION_ID "/answers")
+                                    :request-method :post
+                                    :content-type "application/json"
+                                    :body (json/generate-string the-answer))
+                  response (:response result)
+                  headers (:headers response)]
+              response => (contains {:status 201})
+              headers => (contains {"Location" (contains (str "api/v1/objectives/" OBJECTIVE_ID
+                                                              "/questions/" QUESTION_ID
+                                                              "/answers/" ANSWER_ID))})))))
--- src/objective8/handlers/api.clj
+++ src/objective8/handlers/api.clj
@@ -135,8 +135,13 @@
 (defn post-answer [{:keys [route-params params] :as request}]
   (let [q-id (-> (:q-id route-params)
                  Integer/parseInt)
+        objective-id (:id route-params)
         answer (-> params
                    (select-keys [:answer :created-by-id])
                    (assoc :question-id q-id))
-        stored-answer (answers/store-answer! answer)]
-    (successful-post-response "" stored-answer)))
+        stored-answer (answers/store-answer! answer)
+        resource-location (str utils/host-url
+                               "/api/v1/objectives/" objective-id
+                               "/questions/" (:question-id stored-answer)
+                               "/answers/" (:_id stored-answer))]
+    (successful-post-response resource-location stored-answer)))
