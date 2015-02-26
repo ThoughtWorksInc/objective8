59664f29dc4b2321bcf7d568c0963785a4e12da8
Rob Speller <rspeller@thoughtworks.com>
2015-02-26 11:48:47 +0000

    RS + MG #16 Remove some whitespace

--- test/objective8/writers_test.clj
+++ test/objective8/writers_test.clj
@@ -6,18 +6,18 @@
 
 (fact "Postgresql exceptions are not caught"
       (against-background
-        (utils/generate-random-uuid) => "random-uuid")   
+        (utils/generate-random-uuid) => "random-uuid")
       (writers/store-invitation! {:writer-name "something"}) => (throws org.postgresql.util.PSQLException)
       (provided
-        (storage/pg-store! 
-          {:entity :invitation 
-           :writer-name "something" 
-           :uuid "random-uuid" 
+        (storage/pg-store!
+          {:entity :invitation
+           :writer-name "something"
+           :uuid "random-uuid"
            :status "active"}) =throws=> (org.postgresql.util.PSQLException.
                                           (org.postgresql.util.ServerErrorMessage. "" 0))))
 
 (fact "stores invited writer with random uuid"
-      (writers/store-invitation! {:writer-name "something"}) => :stored-invitation 
+      (writers/store-invitation! {:writer-name "something"}) => :stored-invitation
       (provided
         (utils/generate-random-uuid) => "random-uuid"
         (storage/pg-store! {:entity :invitation :writer-name "something" :uuid "random-uuid" :status "active"}) => :stored-invitation))
