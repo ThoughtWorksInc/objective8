c29033421c5b4efca31fb65b716abf50236e5d9e
Chris Cheshire <chris.cheshire@gmail.com>
2015-03-02 17:22:28 +0000

    Add express server to quickly view jade templates
    Run 'node dev-app.js'
    View on localhost:1234

---
+++ dev-app.js
@@ -0,0 +1,40 @@
+var express = require('express');
+var app = express();
+var favicon = require('serve-favicon');
+
+var pageData = {
+  "javascriptsBase": "/public",
+  "stylesheetsBase": "/public",
+  "imagesBase": "/public/images"
+};
+
+app.use('/public', express.static(__dirname + '/resources/public'));
+
+app.use(favicon('./resources/public/favicon.ico'));
+
+app.set('view engine', 'jade');
+app.set('views', './resources/src/jade');
+
+function beforeAllFilter(req, res, next) {
+  app.locals.pretty = true;
+
+  next();
+}
+
+app.all('*', beforeAllFilter);
+
+app.get('/', function (req, res) {
+  res.render('index', pageData);
+});
+
+app.get('/:name', function(req , res){
+  var pageName = req.params.name;
+  res.render(pageName, pageData);
+});
+
+app.get('/:folder/:name', function(req , res){
+  var pageName = req.params.folder + "/" + req.params.name;
+  res.render(pageName, pageData);
+});
+
+app.listen(1234);
