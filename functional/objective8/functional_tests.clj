1f90c76fae9d68fd4656d1bfe4063c583463126a
Rob Speller <rspeller@thoughtworks.com>
2015-03-09 23:35:29 +0000

    RS | #57 | Add full functional test for creating an objective

--- functional/objective8/functional_tests.clj
+++ functional/objective8/functional_tests.clj
@@ -1,26 +1,59 @@
 (ns objective8.functional-tests 
   (:require [midje.sweet :refer :all]
             [org.httpkit.server :refer [run-server]]
-            [clj-webdriver.taxi :as webdriver]
+            [clj-webdriver.taxi :as wd]
             [objective8.core :as core]
+            [objective8.integration-helpers :as integration-helpers]
             [dev-helpers.stub-twitter :refer [stub-twitter-auth-config]]))
 
 (def config-without-twitter (assoc core/app-config :authentication stub-twitter-auth-config))
 
-(facts "some tests" :functional :integration
+(defn wait-for-title [title]
+  (wd/wait-until #(= (wd/title) title) 5000))
+
+(defn screenshot [filename]
+  (wd/take-screenshot :file (str "functional/objective8/screenshots/" 
+                                 filename ".png")))
+
+(facts "About user journeys" :functional :integration
        (against-background 
          [(before :contents (do (core/start-server config-without-twitter)
-                             (webdriver/set-driver! {:browser :firefox})))
-          (after :contents (do (webdriver/quit)
-                            (core/stop-server)))]
-       (fact "a test" :functional  
-             (do (webdriver/to "localhost:8080")
-                 (webdriver/title)) => "Objective[8]"
-             (do (webdriver/click "a[href='/objectives']") 
-                 (webdriver/title)) => "Objectives | Objective[8]")
+                             (wd/set-driver! {:browser :firefox})))
+          (after :contents (do (wd/quit)
+                               (integration-helpers/truncate-tables)
+                               (core/stop-server)))]
+         (fact "can add an objective" :functional  
+               (do (wd/to "localhost:8080")
+                   (wait-for-title "Objective[8]")
+                   (screenshot "01_home_page")
 
-       (fact "another test" :functional  
-             (do (webdriver/to "localhost:8080")
-                 (webdriver/title)) => "Objective[8]"
-             (do (webdriver/click "a[href='/objectives']") 
-                 (webdriver/title)) => "Objectives | Objective[8]")))  
+                   (wd/click "a[href='/objectives']") 
+                   (wait-for-title "Objectives | Objective[8]")
+                   (screenshot "02_objectives_page")
+
+                   (wd/click "a[href='objectives/create']") 
+                   (wait-for-title "Sign in or Sign up | Objective[8]")
+                   (screenshot "03_sign_in_page")
+
+                   (wd/click "button[title='Sign in with twitter']") 
+                   (wait-for-title "Sign up almost there | Objective[8]")
+                   (screenshot "04_sign_up_almost_there")
+                   
+                   (wd/input-text "#username" "funcTestUser123")
+                   (-> "#email-address" 
+                       (wd/input-text "func_test_user@domain.com")
+                       wd/submit) 
+
+                   (screenshot "05_create_objective_page")
+                   (wait-for-title "Create an Objective | Objective[8]")
+
+                   (wd/input-text "#objective-title" "Functional test headline")
+                   (wd/input-text "#objective-goals" "Functional test goal")
+                   (-> "#objective-end-date" 
+                       (wd/input-text "2015-12-25")
+                       wd/submit) 
+
+                   (wait-for-title "Functional test headline | Objective[8]")
+                   (screenshot "06_objective_page")
+                   (wd/title)) 
+               =>  "Functional test headline | Objective[8]"))) 
--- snap.env.sh
+++ snap.env.sh
@@ -2,6 +2,8 @@
 
 export BASE_URI="localhost:8080"
 export APP_PORT="8080"
+export API_BEARER_NAME=functionalTests
+export API_BEARER_TOKEN=functionalTestsToken
 export DB_USER=$SNAP_DB_PG_USER
 export DB_PASSWORD=$SNAP_DB_PG_PASSWORD
 export DB_HOST=$SNAP_DB_PG_HOST
