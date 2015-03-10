919c98d3079a163d85eef4e025d9880b7b7bae90
Duncan Mortimer <dmortime@thoughtworks.com>
2015-03-10 12:31:26 +0000

    DM+CM #22 - add functional tests for creating and accepting writer invitations

--- functional/objective8/functional_tests.clj
+++ functional/objective8/functional_tests.clj
@@ -2,6 +2,7 @@
   (:require [midje.sweet :refer :all]
             [org.httpkit.server :refer [run-server]]
             [clj-webdriver.taxi :as wd]
+            [clj-webdriver.core :as wc]
             [objective8.core :as core]
             [objective8.integration-helpers :as integration-helpers]
             [dev-helpers.stub-twitter :refer [stub-twitter-auth-config]]))
@@ -15,10 +16,13 @@
   (wd/take-screenshot :file (str "functional/objective8/screenshots/" 
                                  filename ".png")))
 
+(defonce journey-state (atom nil))
+
 (facts "About user journeys" :functional
        (against-background 
          [(before :contents (do (core/start-server config-without-twitter)
-                                (wd/set-driver! {:browser :firefox})))
+                                (wd/set-driver! {:browser :firefox})
+                                (reset! journey-state {})))
           (after :contents (do (wd/quit)
                                (integration-helpers/truncate-tables)
                                (core/stop-server)))]
@@ -54,9 +58,68 @@
                         wd/submit) 
 
                     (wait-for-title "Functional test headline | Objective[8]")
+                    (swap! journey-state assoc :objective-url (wd/current-url))
                     (screenshot "06_objective_page")
                     (wd/title)
                     (catch Exception e
-                      (screenshot "ERROR") 
+                      (screenshot "ERROR-Can-add-an-objective") 
                       (throw e)))
-               =>  "Functional test headline | Objective[8]"))) 
+               =>  "Functional test headline | Objective[8]")
+
+         (fact "Can invite a writer" :functional
+               (try (wd/to "localhost:8080/objectives")
+                    (wd/click "a.clj-objective-link")
+                    (screenshot "07_objective_page")
+                    
+                    (wd/click "a#clj-objectives-writers")
+                    (screenshot "08_candidate_writers_page")
+
+                    (wd/input-text "#writer-name" "Functional test writer name")
+                    (-> "#reason"
+                        (wd/input-text "Functional test invitation reason")
+                        wd/submit)
+                    (screenshot "09_objective_with_invitation_flash")
+
+                    (->> (wd/text "div.wrapper>p")
+                         (re-find #"http://.*$")
+                         (swap! journey-state assoc :invitation-url))
+                    {:page-title (wd/title)
+                     :flash-message (wd/text "div.wrapper>p")}
+
+                    (catch Exception e
+                      (screenshot "ERROR-Can-invite-a-writer")
+                      (throw e)))
+               => (contains {:page-title "Functional test headline | Objective[8]"
+                             :flash-message (contains "Your invited writer can accept their invitation")}))
+
+         (fact "Can accept a writer invitation" :functional
+               (try
+                 (wd/to (:invitation-url @journey-state))
+                 (screenshot "10_invitation_url")
+
+                 (wd/submit "#clj-invitation-response-accept")
+                 (screenshot "11_candidate_writers_as_recently_added_writer")
+
+                 (wd/text "span.candidate-name")
+
+                 (catch Exception e
+                   (screenshot "ERROR-Can-accept-a-writer-invitation")
+                   (throw e)))
+               => "Functional Test Writer Name")
+
+         (future-fact "Can submit a draft" :functional
+               (try
+                 (wd/to (str (:objective-url @journey-state) "/edit-draft"))
+                 (screenshot "12_edit_draft_empty")
+                 
+                 (-> "#draft-text-field"
+                     (wd/input-text "Functional test draft title\n===\nSome content")
+                     (wd/submit))
+                 (screenshot "13_submitted_draft")
+                 
+                 (wd/title)
+                 
+                 (catch Exception e
+                   (screenshot "ERROR-Can-submit-a-draft")
+                   (throw e)))
+               => "Current draft | Objective[8]"))) 
