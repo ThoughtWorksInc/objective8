d2fb9d0366577ddbbf1073976909968023c97cbe
Rob Speller <rspeller@thoughtworks.com>
2015-03-10 07:43:49 +0000

    RS | #57 | Use same screen settings as snap for xvfb | Move functional tests bash script to midje directory

--- midje/run_functional_tests.sh
+++ midje/run_functional_tests.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash 
-start-stop-daemon --start -b -x /usr/bin/Xvfb -- :1 -screen 0 1024x768x24
+start-stop-daemon --start -b -x /usr/bin/Xvfb -- :1 -screen 0 1280x1024x16
 API_BEARER_NAME=functionalTests \
 API_BEARER_TOKEN=functionalTestsToken \
 DISPLAY=:1 lein midje :config midje/functional_tests.clj
