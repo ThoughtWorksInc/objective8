52a043cff9b3520c1be0557387e0d73fca8eaf57
Rob Speller <rspeller@thoughtworks.com>
2015-03-10 08:01:51 +0000

    RS | #57 | Catch error and screenshot in functional test

--- functional/objective8/functional_tests.clj
+++ functional/objective8/functional_tests.clj
@@ -18,42 +18,45 @@
 (facts "About user journeys" :functional :integration
        (against-background 
          [(before :contents (do (core/start-server config-without-twitter)
-                             (wd/set-driver! {:browser :firefox})))
+                                (wd/set-driver! {:browser :firefox})))
           (after :contents (do (wd/quit)
                                (integration-helpers/truncate-tables)
                                (core/stop-server)))]
          (fact "can add an objective" :functional  
-               (do (wd/to "localhost:8080")
-                   (wait-for-title "Objective[8]")
-                   (screenshot "01_home_page")
+               (try (wd/to "localhost:8080")
+                    (wait-for-title "Objective[8]")
+                    (screenshot "01_home_page")
 
-                   (wd/click "a[href='/objectives']") 
-                   (wait-for-title "Objectives | Objective[8]")
-                   (screenshot "02_objectives_page")
+                    (wd/click "a[href='/objectives']") 
+                    (wait-for-title "Objectives | Objective[8]")
+                    (screenshot "02_objectives_page")
 
-                   (wd/click "a[href='objectives/create']") 
-                   (wait-for-title "Sign in or Sign up | Objective[8]")
-                   (screenshot "03_sign_in_page")
+                    (wd/click "a[href='objectives/create']") 
+                    (wait-for-title "Sign in or Sign up | Objective[8]")
+                    (screenshot "03_sign_in_page")
 
-                   (wd/click "button[title='Sign in with twitter']") 
-                   (wait-for-title "Sign up almost there | Objective[8]")
-                   (screenshot "04_sign_up_almost_there")
-                   
-                   (wd/input-text "#username" "funcTestUser123")
-                   (-> "#email-address" 
-                       (wd/input-text "func_test_user@domain.com")
-                       wd/submit) 
+                    (wd/click "button[title='Sign in with twitter']") 
+                    (wait-for-title "Sign up almost there | Objective[8]")
+                    (screenshot "04_sign_up_almost_there")
 
-                   (screenshot "05_create_objective_page")
-                   (wait-for-title "Create an Objective | Objective[8]")
+                    (wd/input-text "#username" "funcTestUser123")
+                    (-> "#email-address" 
+                        (wd/input-text "func_test_user@domain.com")
+                        wd/submit) 
 
-                   (wd/input-text "#objective-title" "Functional test headline")
-                   (wd/input-text "#objective-goals" "Functional test goal")
-                   (-> "#objective-end-date" 
-                       (wd/input-text "2015-12-25")
-                       wd/submit) 
+                    (screenshot "05_create_objective_page")
+                    (wait-for-title "Create an Objective | Objective[8]")
 
-                   (wait-for-title "Functional test headline | Objective[8]")
-                   (screenshot "06_objective_page")
-                   (wd/title)) 
+                    (wd/input-text "#objective-title" "Functional test headline")
+                    (wd/input-text "#objective-goals" "Functional test goal")
+                    (-> "#objective-end-date" 
+                        (wd/input-text "2015-12-25")
+                        wd/submit) 
+
+                    (wait-for-title "Functional test headline | Objective[8]")
+                    (screenshot "06_objective_page")
+                    (wd/title)
+                    (catch Exception e
+                      (screenshot "ERROR") 
+                      (throw e)))
                =>  "Functional test headline | Objective[8]"))) 
