e189b51e0bd761cd5402ec7eb4b49f885f1ddd74
Rob Speller <rspeller@thoughtworks.com>
2015-03-18 16:25:21 +0000

    RS & CC | #60 | Translate 'Invite a writer button' and set the link target

--- src/objective8/templates/objective.clj
+++ src/objective8/templates/objective.clj
@@ -6,16 +6,18 @@
 (def objective-template (html/html-resource "templates/jade/objective.html" {:parser jsoup/parser}))
 
 (defn objective-page [{:keys [translations data] :as context}]
-  (let [title (get-in data [:objective :title])
-        description (get-in data [:objective :description])
+  (let [objective (:objective data)
         candidates (:candidates data)]
     (apply str
            (html/emit*
              (html/at objective-template
                       [:title] (html/content (get-in context [:doc :title]))
                       [:.clj-user-navigation-signed-out] (html/substitute (f/user-navigation-signed-in? context))
-                      [:.clj-objective-title] (html/content title)
-                      [:.clj-replace-with-objective-detail] (html/substitute (f/text->p-nodes description))
+                      [:.clj-objective-title] (html/content (:title objective))
+                      [:.clj-replace-with-objective-detail] (html/substitute (f/text->p-nodes (:description objective)))
                       [:.clj-writers-section-title] (html/content (translations :objective-view/writers))
                       [:.clj-writer-item-list] (html/content (f/writer-list context)) 
-                      )))))
+                      [:.clj-invite-writer-link] (html/do-> 
+                                                   (html/set-attr "href" (str "/objectives/" (:_id objective) 
+                                                                              "/candidate-writers"))
+                                                   (html/content (translations :objective-view/invite-a-writer))))))))
