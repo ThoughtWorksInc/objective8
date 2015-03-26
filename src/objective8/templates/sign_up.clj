78524f885fb9462fcc4355fabfafa11407db8c9a
Rob Speller <rspeller@thoughtworks.com>
2015-03-26 10:59:06 +0000

    RS & CC | #64 | Use translator function in sign_up.clj template

--- src/objective8/templates/sign_up.clj
+++ src/objective8/templates/sign_up.clj
@@ -7,7 +7,8 @@
 (def sign-up-template (html/html-resource "templates/jade/sign-up.html" {:parser jsoup/parser}))
 
 (defn sign-up-page [{:keys [translations data doc] :as context}]
-  (let [objective (:objective data)]
+  (let [objective (:objective data)
+        tl8 (f/translator context)]
     (apply str
            (html/emit*
              (html/at sign-up-template 
@@ -16,12 +17,12 @@
                       [:.clj-masthead-signed-out] (html/substitute (f/masthead context))
                       [:.clj-status-bar] (html/substitute (f/status-flash-bar context))
 
-                      [:.l8n-sign-up-title] (html/content (translations :sign-up/page-title))
-                      [:.l8n-sign-up-description] (html/content (translations :sign-up/welcome))
+                      [:.l8n-sign-up-title] (tl8 :sign-up/page-title)
+                      [:.l8n-sign-up-description] (tl8 :sign-up/welcome)
 
                       [:.clj-sign-up-form] (html/prepend (html/html-snippet (anti-forgery-field)))
-                      [:.l8n-label-username] (html/content (translations :sign-up/username-label))
+                      [:.l8n-label-username] (tl8 :sign-up/username-label)
                       [:.clj-username-error] (if-let [error-type (get-in doc [:errors :username])]
-                                               (html/content (translations (keyword "sign-up" (name error-type))))) 
-                      [:.l8n-label-email-address] (html/content (translations :sign-up/email-label))
-                      [:.l8n-sign-up] (html/content (translations :sign-up/button)))))))
+                                               (tl8 (keyword "sign-up" (name error-type))))
+                      [:.l8n-label-email-address] (tl8 :sign-up/email-label)
+                      [:.l8n-sign-up] (tl8 :sign-up/button))))))
