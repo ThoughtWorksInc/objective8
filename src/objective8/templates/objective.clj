728252ae0736891ce9c2f149e4fd136588982eab
Clare Matthews <cmatthew@thoughtworks.com>
2015-03-27 13:51:01 +0000

    AB+CM #12 - add translations for invitation modal

--- src/objective8/templates/objective.clj
+++ src/objective8/templates/objective.clj
@@ -15,6 +15,7 @@
   (html/transformation
     [:.l8n-invitation-guidance-text-line-1] (html/content (translations :invitation/guidance-text-line-1))
     [:.l8n-invitation-guidance-text-line-2] (html/content (translations :invitation/guidance-text-line-2))
+    [:.l8n-invitation-guidance-text-line-3] (html/content (translations :invitation/guidance-text-line-3))
     [:.clj-invitation-url] (html/set-attr "value" (:invitation-url flash))
     [:.clj-mail-to] (html/do->
                       (html/set-attr "href" (mail-to-string flash
