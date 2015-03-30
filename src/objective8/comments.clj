7ffda87b3258f303a0b4c2ec56c3037b51175526
Duncan Mortimer <dmortime@thoughtworks.com>
2015-03-30 10:48:12 +0100

    CM + DM #19 - Remove old method for posting comment on objective

--- src/objective8/comments.clj
+++ src/objective8/comments.clj
@@ -3,7 +3,7 @@
             [objective8.objectives :refer [open?] :as objectives]
             [objective8.utils :as utils]))
 
-(defn replace-global-id [comment comment-on-uri]
+(defn replace-comment-on-id [comment comment-on-uri]
   (-> comment
       (assoc :comment-on-uri comment-on-uri)
       (dissoc :comment-on-id)))
@@ -18,11 +18,10 @@
                    :objective-id (or objective-id _id))
             (dissoc :comment-on-uri)
             storage/pg-store!
-            (replace-global-id comment-on-uri))))
+            (replace-comment-on-id comment-on-uri))))
 
 (defn create-comment-on-objective! [{objective-id :objective-id :as comment}]
-  (when (open? (objectives/retrieve-objective objective-id))
-    (storage/pg-store! (assoc comment :entity :comment))))
+  )
 
 (defn get-comments [entity-uri]
   (when-let [{:keys [global-id]} (storage/pg-retrieve-entity-by-uri entity-uri :with-global-id)]
@@ -32,4 +31,4 @@
                                :sort {:field :_created_at
                                       :ordering :DESC}})
          :result
-         (map #(replace-global-id % entity-uri)))))
+         (map #(replace-comment-on-id % entity-uri)))))
--- test/objective8/unit/comments_test.clj
+++ test/objective8/unit/comments_test.clj
@@ -10,14 +10,3 @@
 
 (def comment {:objective-id OBJECTIVE_ID
               :comment-on-id GLOBAL_ID})
-
-(fact "A comment can be created when the associated objective is not in drafting"
-      (comments/create-comment-on-objective! comment) => :stored-comment
-      (provided
-       (objectives/retrieve-objective OBJECTIVE_ID) => {:drafting-started false}
-       (storage/pg-store! anything) => :stored-comment))
-
-(fact "Attempting to create a comment against an objective that is in drafting returns nil"
-      (comments/create-comment-on-objective! comment) => nil
-      (provided
-        (objectives/retrieve-objective OBJECTIVE_ID) => {:drafting-started true}))
