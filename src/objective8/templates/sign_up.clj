b3fc6ed41a731e2fa1ea99486ce51ba10f9ef461
Rob Speller <rspeller@thoughtworks.com>
2015-04-01 16:27:48 +0100

    RS | #89 | Update sign-up page with auto-translations

--- src/objective8/templates/sign_up.clj
+++ src/objective8/templates/sign_up.clj
@@ -8,23 +8,17 @@
 (def sign-up-template (html/html-resource "templates/jade/sign-up.html" {:parser jsoup/parser}))
 
 (defn sign-up-page [{:keys [translations data doc] :as context}]
-  (let [objective (:objective data)
-        tl8 (tf/translator context)]
+  (let [objective (:objective data)]
     (apply str
            (html/emit*
-             (f/add-google-analytics
-               (html/at sign-up-template 
-                        [:title] (html/content (:title doc))
-                        [(and (html/has :meta) (html/attr= :name "description"))] (html/set-attr "content" (:description doc))
-                        [:.clj-masthead-signed-out] (html/substitute (f/masthead context))
-                        [:.clj-status-bar] (html/substitute (f/status-flash-bar context))
+             (tf/translate context
+                           (f/add-google-analytics
+                             (html/at sign-up-template 
+                                      [:title] (html/content (:title doc))
+                                      [(and (html/has :meta) (html/attr= :name "description"))] (html/set-attr "content" (:description doc))
+                                      [:.clj-masthead-signed-out] (html/substitute (f/masthead context))
+                                      [:.clj-status-bar] (html/substitute (f/status-flash-bar context))
 
-                        [:.l8n-sign-up-title] (tl8 :sign-up/page-title)
-                        [:.l8n-sign-up-description] (tl8 :sign-up/welcome)
-
-                        [:.clj-sign-up-form] (html/prepend (html/html-snippet (anti-forgery-field)))
-                        [:.l8n-label-username] (tl8 :sign-up/username-label)
-                        [:.clj-username-error] (if-let [error-type (get-in doc [:errors :username])]
-                                                 (tl8 (keyword "sign-up" (name error-type))))
-                        [:.l8n-label-email-address] (tl8 :sign-up/email-label)
-                        [:.l8n-sign-up] (tl8 :sign-up/button)))))))
+                                      [:.clj-sign-up-form] (html/prepend (html/html-snippet (anti-forgery-field)))
+                                      [:.clj-username-error] (when-let [error-type (get-in doc [:errors :username])]
+                                                               (html/content (translations (keyword "sign-up" (name error-type))))))))))))
