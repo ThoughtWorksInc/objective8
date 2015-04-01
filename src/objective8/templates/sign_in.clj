a00b7b20ecb27e470f2fbcc8afdca5855e2de2ba
Rob Speller <rspeller@thoughtworks.com>
2015-04-01 16:37:30 +0100

    RS | #89 | Update sign-in page with auto-translations

--- src/objective8/templates/sign_in.clj
+++ src/objective8/templates/sign_in.clj
@@ -2,21 +2,19 @@
   (:require [net.cgrand.enlive-html :as html]
             [net.cgrand.jsoup :as jsoup]
             [ring.util.anti-forgery :refer [anti-forgery-field]]  
-            [objective8.templates.page-furniture :as f]))
+            [objective8.templates.page-furniture :as f]
+            [objective8.templates.template-functions :as tf]))
 
 (def sign-in-template (html/html-resource "templates/jade/sign-in.html" {:parser jsoup/parser}))
 
-(defn sign-in-page [{:keys [translations data doc] :as context}]
+(defn sign-in-page [{:keys [data doc] :as context}]
   (let [objective (:objective data)]
     (apply str
            (html/emit*
-             (f/add-google-analytics
-               (html/at sign-in-template 
-                        [:title] (html/content (:title doc))
-                        [(and (html/has :meta) (html/attr= :name "description"))] (html/set-attr "content" (:description doc))
-                        [:.clj-masthead-signed-out] (html/substitute (f/masthead context))
-                        [:.clj-status-bar] (html/substitute (f/status-flash-bar context))
-
-                        [:.l8n-sign-in-title] (html/content (translations :sign-in/page-title))
-                        [:.l8n-sign-in-with-twitter] (html/content (translations :sign-in/sign-in-with-twitter))
-                        [:.l8n-twitter-help-text] (html/content (translations :sign-in/twitter-help-text))))))))
+             (tf/translate context
+                           (f/add-google-analytics
+                             (html/at sign-in-template 
+                                      [:title] (html/content (:title doc))
+                                      [(and (html/has :meta) (html/attr= :name "description"))] (html/set-attr "content" (:description doc))
+                                      [:.clj-masthead-signed-out] (html/substitute (f/masthead context))
+                                      [:.clj-status-bar] (html/substitute (f/status-flash-bar context)))))))))
