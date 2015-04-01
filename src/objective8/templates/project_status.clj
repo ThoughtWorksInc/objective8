f31ac35362737c62dc28c5d4c2a8002554b88c00
Rob Speller <rspeller@thoughtworks.com>
2015-04-01 17:04:36 +0100

    RS | #89 | Update project-status page with auto-translations

--- src/objective8/templates/project_status.clj
+++ src/objective8/templates/project_status.clj
@@ -1,5 +1,6 @@
 (ns objective8.templates.project-status 
   (:require [net.cgrand.enlive-html :as html]
+            [objective8.templates.template-functions :as tf]   
             [objective8.templates.page-furniture :as f])) 
 
 (def project-status-template (html/html-resource "templates/jade/project-status.html"))
@@ -7,14 +8,10 @@
 (defn project-status-page [{:keys [translations doc] :as context}]
   (apply str
          (html/emit*
-           (f/add-google-analytics
-             (html/at project-status-template
-                      [:title] (html/content (:title doc))
-                      [(and (html/has :meta) (html/attr= :name "description"))] (html/set-attr "content" (:description doc))
-                      [:.clj-masthead-signed-out] (html/substitute (f/masthead context))
-                      [:.clj-status-bar] (html/substitute (f/status-flash-bar context))
-
-                      [:.l8n-project-status-heading] (html/content (translations :project-status/page-title)) 
-
-                      [:.l8n-project-status-lede] (html/content (translations :project-status/page-intro))
-                      [:.l8n-project-status-detail] (html/html-content (translations :project-status/page-content)))))))
+           (tf/translate context
+                         (f/add-google-analytics
+                           (html/at project-status-template
+                                    [:title] (html/content (:title doc))
+                                    [(and (html/has :meta) (html/attr= :name "description"))] (html/set-attr "content" (:description doc))
+                                    [:.clj-masthead-signed-out] (html/substitute (f/masthead context))
+                                    [:.clj-status-bar] (html/substitute (f/status-flash-bar context))))))))
