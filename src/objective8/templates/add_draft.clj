493213de09bb55d270f9a794a1d0b0ada1571d8b
Rob Speller <rspeller@thoughtworks.com>
2015-04-01 16:05:53 +0100

    RS | #89 | Update add-draft page with auto-translations

--- src/objective8/templates/add_draft.clj
+++ src/objective8/templates/add_draft.clj
@@ -1,29 +1,28 @@
 (ns objective8.templates.add-draft
   (:require [net.cgrand.enlive-html :as html]
             [net.cgrand.jsoup :as jsoup]
+            [ring.util.anti-forgery :refer [anti-forgery-field]]  
             [objective8.templates.page-furniture :as f]
-            [ring.util.anti-forgery :refer [anti-forgery-field]]))
+            [objective8.templates.template-functions :as tf]))
 
 (def add-draft-template (html/html-resource "templates/jade/add-draft.html" {:parser jsoup/parser}))
 
-(defn add-draft-page [{:keys [translations data doc] :as context}]
+(defn add-draft-page [{:keys [data doc] :as context}]
   (apply str
          (html/emit*
-           (f/add-google-analytics
-             (html/at add-draft-template
-                      [:title] (html/content (:title doc))
-                      [(and (html/has :meta) (html/attr= :name "description"))] (html/set-attr "content" (:description doc))
-                      [:.clj-masthead-signed-out] (html/substitute (f/masthead context))
-                      [:.clj-status-bar] (html/substitute (f/status-flash-bar context))
+           (tf/translate context
+                         (f/add-google-analytics
+                           (html/at add-draft-template
+                                    [:title] (html/content (:title doc))
+                                    [(and (html/has :meta) (html/attr= :name "description"))] (html/set-attr "content" (:description doc))
+                                    [:.clj-masthead-signed-out] (html/substitute (f/masthead context))
+                                    [:.clj-status-bar] (html/substitute (f/status-flash-bar context))
 
-                      [:.clj-add-draft-preview] (when-let [preview (:preview data)]
-                                                  (html/html-content preview))
+                                    [:.clj-add-draft-preview] (when-let [preview (:preview data)]
+                                                                (html/html-content preview))
 
-                      [:.clj-add-draft-form] (html/do-> 
-                                               (html/set-attr :action (str "/objectives/" (:objective-id data) "/add-draft")) 
-                                               (html/prepend (html/html-snippet (anti-forgery-field)))) 
-                      [:.clj-add-draft-content] (html/do-> 
-                                                  (html/set-attr :placeholder (translations :add-draft/placeholder))
-                                                  (html/content (:markdown data))) 
-                      [:.l8n-preview-action] (html/content (translations :add-draft/preview))
-                      [:.l8n-submit-action] (html/content (translations :add-draft/submit)))))))
+                                    [:.clj-add-draft-form] (html/do-> 
+                                                             (html/set-attr :action (str "/objectives/" (:objective-id data) "/add-draft")) 
+                                                             (html/prepend (html/html-snippet (anti-forgery-field)))) 
+                                    [:.clj-add-draft-content] (html/content (:markdown data))
+                                    ))))))
