f622f220125eeaaf1395d6dce7107d535df762b2
Rob Speller <rspeller@thoughtworks.com>
2015-04-05 22:13:53 +0100

    RS | #0 | Add db connection and truncate tables to up-down-votes integration tests as they keep breaking on CI

--- test/objective8/integration/db/up_down_votes.clj
+++ test/objective8/integration/db/up_down_votes.clj
@@ -5,16 +5,20 @@
             [objective8.integration.storage-helpers :as sh]))
 
 (facts "about storing votes"
+       (against-background 
+         [(before :contents (do (ih/db-connection)
+                                (ih/truncate-tables)))
+          (after :facts (ih/truncate-tables))] 
 
-       (fact "votes can be stored against an answer"
-             (let [answer-to-vote-on (sh/store-an-answer)
-                   {voting-user-id :_id} (sh/store-a-user)
-                   uri "answer-uri"
-                   vote-data {:vote-on-uri uri :created-by-id voting-user-id :vote-type :up}
-                   stored-vote (votes/store-vote! answer-to-vote-on vote-data)]
-               stored-vote => (contains {:_id integer?
-                                         :vote-type :up
-                                         :created-by-id voting-user-id
-                                         :entity :up-down-vote
-                                         :vote-on-uri "answer-uri"})
-               stored-vote =not=> (contains {:global-id anything}))))
+         (fact "votes can be stored against an answer"
+               (let [answer-to-vote-on (sh/store-an-answer)
+                     {voting-user-id :_id} (sh/store-a-user)
+                     uri "answer-uri"
+                     vote-data {:vote-on-uri uri :created-by-id voting-user-id :vote-type :up}
+                     stored-vote (votes/store-vote! answer-to-vote-on vote-data)]
+                 stored-vote => (contains {:_id integer?
+                                           :vote-type :up
+                                           :created-by-id voting-user-id
+                                           :entity :up-down-vote
+                                           :vote-on-uri "answer-uri"})
+                 stored-vote =not=> (contains {:global-id anything})))))
