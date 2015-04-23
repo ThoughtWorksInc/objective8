84e97ad887d910f7044ce159a0ac1c65bc58edde
Rob Speller <rspeller@thoughtworks.com>
2015-04-23 19:05:16 +0100

    RS & AB | #33 | Diff drafts with hiccup structure maintained - still need to deal with empty elements

--- src/objective8/draft_diffs_new.clj
+++ src/objective8/draft_diffs_new.clj
@@ -3,6 +3,7 @@
             [objective8.utils :as utils]
             [objective8.drafts :as drafts]))
 
+(declare replacement-element-and-updated-diff)
 
 (defn hiccup->vector-of-strings [strings hcp] 
   (let [first-element (first hcp)] 
@@ -33,105 +34,24 @@
 (defn remove-hiccup-elements [hiccup element]
   (filter #(not= element (first %)) hiccup))
 
-(defn is-attribute? [data]
-  (or (nil? data) (instance? clojure.lang.PersistentArrayMap data)))
-
-(defn get-element-content [element]
- (if (is-attribute? (second element))
-   (drop 2 element)
-   (drop 1 element)))
-
-(defn split-element-at-position [element char-position]
-  (let [element-type (first element)
-        element-content (first (get-element-content element))]
-   [[element-type (subs element-content 0 char-position)] 
-    [element-type (subs element-content char-position)]]))
-
-
-(defn wrap-with-tag [elements tag]
-  (apply merge [tag] elements))
-
-(defn get-char-position [index paragraph-size cumulative-sum]
-  (if (= 0 index)
-    paragraph-size
-    (- paragraph-size (nth cumulative-sum (dec index)))))
-
-(defn get-elements-to-format [diffs index cumulative-sum paragraph-size]
-  (let [complete-elements (take index diffs)
-        char-position (get-char-position index paragraph-size cumulative-sum)
-        final-element (if (> (nth cumulative-sum index) paragraph-size)
-                       (first (split-element-at-position (nth diffs index) char-position)) 
-                       (nth diffs index))]
-    (concat complete-elements [final-element])))
-
-(defn get-remaining-char-count [diff-char-count index cumulative-sum paragraph-size]
-  (let [new-char-count (drop index diff-char-count)
-        char-position (get-char-position index paragraph-size cumulative-sum)]
-    (->> (rest new-char-count)
-         (list* (- (first new-char-count) char-position)) 
-         (remove #(= 0 %)))))
-
-(defn get-remaining-diffs [diffs index cumulative-sum paragraph-size]
-  (let [new-diffs (drop (inc index) diffs)
-        char-position (if (= 0 index)
-                        paragraph-size
-                        (- paragraph-size (nth cumulative-sum (dec index))))]
-    (if (> (nth cumulative-sum index) paragraph-size)
-      (list* (second (split-element-at-position (nth diffs index) char-position)) new-diffs) 
-      new-diffs)))
-
-
-(defn format-diff [{:keys [draft-tag-types diff-char-count draft-char-count formatted-elements diffs] :as data}]
- (let [paragraph-size (first draft-char-count) 
-     ;  paragraph-size (min (first (:draft-char-count data)) (reduce + diff-char-count))
-       cumulative-sum (reductions + diff-char-count)
-       index (count (filter #(< % paragraph-size) cumulative-sum))
-       elements-to-format (get-elements-to-format diffs index cumulative-sum paragraph-size)]
-   {:formatted-elements (into formatted-elements [(wrap-with-tag elements-to-format (first draft-tag-types))])
-    :draft-tag-types (rest draft-tag-types)
-    :diff-char-count (get-remaining-char-count diff-char-count index cumulative-sum paragraph-size)
-    :draft-char-count (drop 1 (:draft-char-count data))
-    :diffs (get-remaining-diffs diffs index cumulative-sum paragraph-size)}))
-
-
-(defn get-types-for-element [element]
-  (let [element-content (get-element-content element)]
-    (if (string? (first element-content))
-      (first element)
-      (map get-types-for-element element-content))))
-
-(defn get-types-for-hiccup [text]
-  (-> (map get-types-for-element text)
-      flatten))
-
-(defn get-char-count-for-element [element]
-  (let [element-content (get-element-content element)]
-    (if (string? (first element-content)) 
-      (count (first element-content))
-      (map get-char-count-for-element element-content))))
-
-(defn get-char-counts-for-hiccup [text]
-  (->> (map get-char-count-for-element text)
-       (remove #(= 0 %))
-       flatten))
-
 (defn replacement-diff-for-n-chars [n diff]
-  (let [diff-element (first diff)
-        diff-element-string (last diff-element) 
-        diff-element-char-count (count diff-element-string)]
-    (if (<= n diff-element-char-count)
-      (let [extracted-diff-element (assoc diff-element 2 (subs diff-element-string 0 n )) 
-            remaining-diff-element-string (subs diff-element-string n)
-            updated-diff-element (if (empty? remaining-diff-element-string)
-                                   '() 
-                                   (list (assoc diff-element 2 remaining-diff-element-string)))]
-        {:replacement (list extracted-diff-element) 
-         :updated-diff (concat updated-diff-element (rest diff)) })
-      (let [recursive-result (replacement-diff-for-n-chars (- n diff-element-char-count) (rest diff))
-            recursive-replacement (:replacement recursive-result)
-            recursive-updated-diff (:updated-diff recursive-result)]
-        {:replacement (into [] (concat (list diff-element) recursive-replacement)) 
-         :updated-diff recursive-updated-diff}))))
+  
+    (let [diff-element (first diff)
+          diff-element-string (last diff-element) 
+          diff-element-char-count (count diff-element-string)]
+      (if (<= n diff-element-char-count)
+        (let [extracted-diff-element (conj (pop diff-element) (subs diff-element-string 0 n)) 
+              remaining-diff-element-string (subs diff-element-string n)
+              updated-diff-element (if (empty? remaining-diff-element-string)
+                                     '() 
+                                     (list (conj (pop diff-element) remaining-diff-element-string)))]
+          {:replacement (list extracted-diff-element) 
+           :updated-diff (concat updated-diff-element (rest diff)) })
+        (let [recursive-result (replacement-diff-for-n-chars (- n diff-element-char-count) (rest diff))
+              recursive-replacement (:replacement recursive-result)
+              recursive-updated-diff (:updated-diff recursive-result)]
+          {:replacement (into [] (concat (list diff-element) recursive-replacement)) 
+           :updated-diff recursive-updated-diff}))))
 
 (defn replacement-content-and-updated-diff [content diff]
   ;; Each thing in content will be a string or a vector (another html element)
@@ -147,38 +67,33 @@
            :updated-diff recursive-updated-diff})
 
         ;; else first-element must be a nested-tag vector
-        (let [{:keys [replacement-element updated-diff]} (get-replacement-element-and-updated-diff first-element diff)
+        (let [{:keys [replacement-element updated-diff]} (replacement-element-and-updated-diff first-element diff)
               {recursive-replacement :replacement-content 
                recursive-updated-diff :updated-diff} (replacement-content-and-updated-diff (rest content) updated-diff)] 
           {:replacement-content (into [] (concat [replacement-element] recursive-replacement))
            :updated-diff recursive-updated-diff})))))
 
-(defn get-replacement-element-and-updated-diff [element diff] 
-  (let [{:keys [replacement-content updated-diff]} (replacement-content-and-updated-diff (subvec element 2) diff)]
-    {:replacement-element (into [] (concat (subvec element 0 2) replacement-content)) 
+(defn split-element-content [element]
+  (let [attrs (first (rest element))]
+    (if (or (map? attrs) (nil? attrs))
+      {:element-without-content (subvec element 0 2)
+       :content (subvec element 2)}
+      {:element-without-content (subvec element 0 1)
+       :content (subvec element 1)})))
+
+(defn replacement-element-and-updated-diff [element diff] 
+  (let [{:keys [element-without-content content]} (split-element-content element) 
+        {:keys [replacement-content updated-diff]} (replacement-content-and-updated-diff content diff)]
+    {:replacement-element (into [] (concat element-without-content replacement-content)) 
      :updated-diff updated-diff})) 
 
 (defn insert-diffs-into-drafts [diffs draft]
   (if (empty? draft)
     draft
     (let [first-draft-element (first draft)
-          {:keys [replacement-element updated-diff]} (get-replacement-element-and-updated-diff first-draft-element diffs)
+          {:keys [replacement-element updated-diff]} (replacement-element-and-updated-diff first-draft-element diffs)
           recursive-returned-draft (insert-diffs-into-drafts updated-diff (rest draft))]
       (concat (list replacement-element) recursive-returned-draft))))
-
-(defn add-formatting [diffs draft]
-  (def formatted-draft (atom {:formatted-elements []  
-                              :draft-tag-types (get-types-for-hiccup draft)
-                              :diff-char-count (get-char-counts-for-hiccup diffs)
-                              :draft-char-count (get-char-counts-for-hiccup draft)
-                              :diffs diffs}))
-  (while (not-empty (:draft-char-count @formatted-draft))
-    (swap! formatted-draft format-diff))
-  (->> @formatted-draft
-       :formatted-elements
-       (into '())
-       reverse))
-
 
 (defn diff-hiccup-content [hiccup-1 hiccup-2]
   (-> (dmp/diff (remove-tags hiccup-1) (remove-tags hiccup-2))
--- test/objective8/unit/draft_diffs_test_new.clj
+++ test/objective8/unit/draft_diffs_test_new.clj
@@ -2,7 +2,7 @@
   (:require [midje.sweet :refer :all]
             [objective8.draft-diffs-new :as diffs]))
 
-(def HICCUP_1 '(["p" {} "First paragraph."] ["ul" {} ["li" {} "List item 1."] ["li" {} "List item 2."] ["li" {} "List item 3."]] ["p" {} "Last paragraph."]))
+(def HICCUP_1 '(["p" nil "First paragraph."] ["ul" ["li" "List item 1."] ["li" "List item 2."] ["li" "List item 3."]] ["p" {} "Last paragraph."]))
 (def HICCUP_2 '(["p" {} "First paragraph."] ["p" {} "Second paragraph."] ["p" {} "Third paragraph."]))
 (def HICCUP_3 '(["p" {} "First paragraph."] ["p" {} "New paragraph."] ["p" {} "Third paragraph."]))
 
@@ -15,7 +15,7 @@
 (def FORMATTED_DIFF_2 '(["p" [:span nil "First paragraph."]] ["p" [:del nil "Second paragraph."]] ["p" [:span nil "Third paragraph."]]))
 (def FORMATTED_PREVIOUS_DIFF_2_vs_3 '(["p" {} [:span nil "First paragraph."]] ["p" {} [:del nil "Second"] [:span nil " paragraph."]] ["p" {} [:span nil "Third paragraph."]]))
 (def FORMATTED_CURRENT_DIFF_2_vs_3 '(["p" {} [:span nil "First paragraph."]] ["p" {} [:ins nil "New"] [:span nil " paragraph."]] ["p" {} [:span nil "Third paragraph."]]))
-(def FORMATTED_PREVIOUS_DIFF_1_vs_2 '(["p" {} [:span nil "First paragraph."]] ["ul" {} ["li" {} [:del nil "List item 1."]] ["li" {} [:del nil "List item 2."]] ["li" {} [:del nil "List item 3."]]] ["p" {} [:del nil "Last"] [:span nil " paragraph."]]))
+(def FORMATTED_PREVIOUS_DIFF_1_vs_2 '(["p" nil [:span nil "First paragraph."]] ["ul" ["li" [:del nil "List item 1."]] ["li" [:del nil "List item 2."]] ["li" [:del nil "List item 3."]]] ["p" {} [:del nil "Last"] [:span nil " paragraph."]]))
 (def FORMATTED_CURRENT_DIFF_1_vs_2 '(["p" {} [:span nil "First paragraph."]]  ["p" {} [:ins nil "Second paragraph."]] ["p" {} [:ins nil "Third"] [:span nil " paragraph."]]))
 
 (fact "Tags are removed from a simple hiccup"
@@ -24,32 +24,10 @@
 (fact "Tags are removed from hiccup"
       (diffs/remove-tags HICCUP_1) => "First paragraph.List item 1.List item 2.List item 3.Last paragraph.")
 
-(fact "Characters in the hiccup element are counted"
-      (diffs/get-char-count-for-element ELEMENT) => 32)
-
-(fact "Characters in hiccup are counted"
-      (diffs/get-char-counts-for-hiccup HICCUP_1) => '(16 12 12 12 15))
-
-(fact "Hiccup element is divided at character position"
-      (diffs/split-element-at-position ELEMENT 16) => [[:del "First sentence. "] [:del "Second sentence."]])
-
 (fact "Diff elements are removed"
       (diffs/remove-hiccup-elements DIFF_1 :ins) => '([:span nil "Equal text. "])
       (diffs/remove-hiccup-elements DIFF_2 :del) => '([:span nil "First paragraph."] [:span nil "Third paragraph."]))
 
-(fact "Diff elements are wrapped in paragraph tags"
-      (diffs/format-diff {:formatted-elements []
-                          :draft-tag-types '(:p)
-                          :diff-char-count '(12 9)
-                          :draft-char-count '(21)
-                          :diffs DIFF_1}) => {:formatted-elements FORMATTED_DIFF_1
-                                              :draft-tag-types '()
-                                              :diff-char-count '()
-                                              :draft-char-count '()
-                                              :diffs '()})
-
-(fact "Diffs are formatted into a draft"
-      (diffs/add-formatting DIFF_2 HICCUP_2) => FORMATTED_DIFF_2)
 
 (fact "Difference between drafts 2 and 3 is returned"
       (diffs/get-diffs-between-drafts {:content HICCUP_3} {:content HICCUP_2}) => {:previous-draft-diffs FORMATTED_PREVIOUS_DIFF_2_vs_3
@@ -58,9 +36,6 @@
 (fact "Difference between drafts 1 and 2 is returned"
       (diffs/get-diffs-between-drafts {:content HICCUP_2} {:content HICCUP_1}) => {:previous-draft-diffs FORMATTED_PREVIOUS_DIFF_1_vs_2
                                                                                    :current-draft-diffs FORMATTED_CURRENT_DIFF_1_vs_2})
-
-(fact "Diff tag types are extracted"
-      (diffs/get-types-for-hiccup HICCUP_1) => ["p" "li" "li" "li" "p"])
 
 (fact "Insert diffs into drafts 1"
       (diffs/insert-diffs-into-drafts '([:span nil "First paragraph."]) '(["p" {} "First paragraph."])) => '(["p" {} [:span nil "First paragraph."]])) 
@@ -87,25 +62,25 @@
        :updated-diff '([:ins nil "orld"])})
 
 (fact "Get replacement element and updated diff 1"
-      (diffs/get-replacement-element-and-updated-diff ["h1" {} "First"] '([:span nil "First"])) => 
+      (diffs/replacement-element-and-updated-diff ["h1" {} "First"] '([:span nil "First"])) => 
                                                       {:replacement-element ["h1" {} [:span nil "First"]]
                                                        :updated-diff '()})
 (fact "Get replacement element and updated diff 2"
-      (diffs/get-replacement-element-and-updated-diff ["h1" {} "First"] '([:span nil "First"] [:span nil " more text"])) => 
+      (diffs/replacement-element-and-updated-diff ["h1" {} "First"] '([:span nil "First"] [:span nil " more text"])) => 
                                                       {:replacement-element ["h1" {} [:span nil "First"]]
                                                        :updated-diff '([:span nil " more text"])})
 
 (fact "Get replacement element and updated diff 3"
-      (diffs/get-replacement-element-and-updated-diff ["h1" {} "First"] '([:span nil "First more text"])) =>
+      (diffs/replacement-element-and-updated-diff ["h1" {} "First"] '([:span nil "First more text"])) =>
                                                       {:replacement-element ["h1" {} [:span nil "First"]]
                                                        :updated-diff '([:span nil " more text"])})
 
 (fact "Get replacement element and updated diff 4"
-      (diffs/get-replacement-element-and-updated-diff ["h1" {} "First element"] '([:span nil "First"] [:ins nil " element"])) =>
+      (diffs/replacement-element-and-updated-diff ["h1" {} "First element"] '([:span nil "First"] [:ins nil " element"])) =>
                                                       {:replacement-element ["h1" {} [:span nil "First"] [:ins nil " element"]]
                                                        :updated-diff '()}) 
 
 (fact "Get replacement element and updated diff 5"
-      (diffs/get-replacement-element-and-updated-diff ["h1" {} "First element"] '([:span nil "First"] [:ins nil " element"] [:span nil " more stuff"])) =>
+      (diffs/replacement-element-and-updated-diff ["h1" {} "First element"] '([:span nil "First"] [:ins nil " element"] [:span nil " more stuff"])) =>
                                                       {:replacement-element ["h1" {} [:span nil "First"] [:ins nil " element"]]
                                                        :updated-diff '([:span nil " more stuff"])})
