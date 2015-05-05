e84138261ebcbaca0c76783b4703d0b7232b1c36
Christoph Wong <chwong@thoughtworks.com>
2015-05-05 11:23:32 +0100

    CW + CL | #117 | Add API test for storing writer notes against comments

--- test/objective8/integration/api/writer_notes.clj
+++ test/objective8/integration/api/writer_notes.clj
@@ -25,7 +25,8 @@
          [(before :contents (do (helpers/db-connection)
                                 (helpers/truncate-tables)))
           (after :facts (helpers/truncate-tables))]
-         (fact "the posted note is stored"
+         
+         (fact "the posted note is stored against an answer"
                (against-background
                    (actions/writer-for-objective? anything anything) => true)
                (let [{user-id :_id :as user} (sh/store-a-user)
@@ -42,6 +43,30 @@
                  (:body response) => (helpers/json-contains {:_id integer?
                                                              :uri (contains "/notes/")
                                                              :note-on-uri uri-for-answer 
+                                                             :note "A note"
+                                                             :objective-id o-id
+                                                             :created-by-id user-id}) 
+                 (:body response) =not=> (helpers/json-contains {:note-on-id anything}) 
+                 (:body response) =not=> (helpers/json-contains {:global-id anything}) 
+                 (:headers response) => (helpers/location-contains (str "/api/v1/meta/writer-notes/"))))
+         
+         (fact "the posted note is stored against a comment"
+               (against-background
+                   (actions/writer-for-objective? anything anything) => true)
+               (let [{user-id :_id :as user} (sh/store-a-user)
+                     {o-id :objective-id c-id :_id :as comment} (sh/store-a-comment)
+                     uri-for-comment (str "/comments/" c-id)
+                     note-data {:note-on-uri uri-for-comment
+                                :note "A note"
+                                :created-by-id user-id}
+                     {response :response} (p/request app (str "/api/v1/meta/writer-notes")
+                                                     :request-method :post
+                                                     :content-type "application/json"
+                                                     :body (json/generate-string note-data))]
+                 (:status response) => 201
+                 (:body response) => (helpers/json-contains {:_id integer?
+                                                             :uri (contains "/notes/")
+                                                             :note-on-uri uri-for-comment 
                                                              :note "A note"
                                                              :objective-id o-id
                                                              :created-by-id user-id}) 
