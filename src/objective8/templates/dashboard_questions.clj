b49c60ba16e07f1a96d7eae73de0189b2c26f947
Duncan Mortimer <dmortime@thoughtworks.com>
2015-05-05 14:27:28 +0100

    AB + DM #115 - saved-item to writer-note in jade, scss, clj templates for questions dashboard answer items

--- src/objective8/templates/dashboard_questions.clj
+++ src/objective8/templates/dashboard_questions.clj
@@ -12,33 +12,32 @@
 (def dashboard-questions-no-answers-snippet (html/select pf/library-html-resource
                                                          [:.clj-dashboard-no-answer-item]))
 
-(def answer-with-no-saved-item-snippet (html/select pf/library-html-resource [:.clj-library-key--dashboard-answer-with-no-saved-item]))
+(def answer-with-no-writer-note-snippet (html/select pf/library-html-resource [:.clj-library-key--dashboard-answer-without-writer-note]))
 
-(def answer-with-saved-item-snippet (html/select pf/library-html-resource [:.clj-library-key--dashboard-answer-with-saved-item]))
+(def answer-with-writer-note-snippet (html/select pf/library-html-resource [:.clj-library-key--dashboard-answer-with-writer-note]))
 
-(def no-saved-item-snippet (html/select answer-with-no-saved-item-snippet [:.clj-dashboard-answer-item]))
-(def saved-item-snippet (html/select answer-with-saved-item-snippet [:.clj-dashboard-answer-item]))
+(def no-writer-note-snippet (html/select answer-with-no-writer-note-snippet [:.clj-dashboard-answer-item]))
+(def writer-note-snippet (html/select answer-with-writer-note-snippet [:.clj-dashboard-answer-item]))
 
 (defn render-answer-without-note [{:keys [ring-request] :as context} answer]
-  (html/at no-saved-item-snippet 
+  (html/at no-writer-note-snippet 
            [:.clj-dashboard-answer-item-text] (html/content (:answer answer)) 
            [:.clj-dashboard-answer-item-up-count] (html/content (str (get-in answer [:votes :up]))) 
            [:.clj-dashboard-answer-item-down-count] (html/content (str (get-in answer [:votes :down]))) 
            [:.clj-refer] (html/set-attr :value (:uri ring-request)) 
            [:.clj-note-on-uri] (html/set-attr :value (:uri answer))
-           [:.clj-dashboard-answer-item-save] (html/prepend (html/html-snippet (anti-forgery-field)))
-           [:.clj-dashboard-answer-item-save] identity))
+           [:.clj-dashboard-answer-item-writer-note-form] (html/prepend (html/html-snippet (anti-forgery-field)))))
 
 (defn render-answer-with-note [context answer]
-  (html/at saved-item-snippet
+  (html/at writer-note-snippet
            [:.clj-dashboard-answer-item-text] (html/content (:answer answer)) 
            [:.clj-dashboard-answer-item-up-count] (html/content (str (get-in answer [:votes :up]))) 
            [:.clj-dashboard-answer-item-down-count] (html/content (str (get-in answer [:votes :down]))) 
-           [:.clj-dashboard-answer-item-saved-content] (html/content (:note answer))))
+           [:.clj-dashboard-answer-item-writer-note-text] (html/content (:note answer))))
 
 (defn answer-list-items [{:keys [data] :as context}]
   (let [answers (:answers data)]
-    (html/at answer-with-no-saved-item-snippet
+    (html/at answer-with-no-writer-note-snippet
              [:.clj-dashboard-answer-item]
              (html/clone-for [answer answers]
                              [:.clj-dashboard-answer-item]  (if (:note answer)
