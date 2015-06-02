e304f611d9f1a0b949e3f3fd8cafbfc2b00e155a
Rob Speller <rspeller@thoughtworks.com>
2015-06-02 17:56:56 +0100

    RS + CW | #124 | Add shell script for starting app as part of deployment

---
+++ init-script/remote_start_objective8.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+[ -f /etc/init.d/objective8d ] && sudo service objective8d stop || true
+mkdir -p /usr/local/objective8
+cp objective8-0.0.1-SNAPSHOT-standalone.jar /usr/local/objective8
+cp -r migrations /usr/local/objective8
+sudo cp objective8d /etc/init.d
+sudo service objective8d start
