d1461987ca99fa1e2cfbbf4fa097a23ed58b6faf
Rob Speller <rspeller@thoughtworks.com>
2015-06-03 11:43:28 +0100

    RS & AB | #124 | Edit objective8d to source config vars

--- init-script/remote_start_objective8.sh
+++ init-script/remote_start_objective8.sh
@@ -4,4 +4,4 @@ mkdir -p /usr/local/objective8
 cp objective8-0.0.1-SNAPSHOT-standalone.jar /usr/local/objective8
 cp -r migrations /usr/local/objective8
 sudo cp objective8d /etc/init.d
-DB_PASSWORD=$DB_PASSWORD sudo service objective8d start
+sudo service objective8d start
