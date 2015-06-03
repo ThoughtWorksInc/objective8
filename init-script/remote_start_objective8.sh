559effa09a61f9e6de79978c95b2e4d8b89bd2e4
Rob Speller <rspeller@thoughtworks.com>
2015-06-03 16:07:15 +0100

    RS + AB | #124 | Update deploy script for Digital Ocean

--- init-script/remote_start_objective8.sh
+++ init-script/remote_start_objective8.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 [ -f /etc/init.d/objective8d ] && sudo service objective8d stop || true
 mkdir -p /usr/local/objective8
-cp objective8-0.0.1-SNAPSHOT-standalone.jar /usr/local/objective8
-cp -r migrations /usr/local/objective8
-sudo cp objective8d /etc/init.d
+mv objective8-0.0.1-SNAPSHOT-standalone.jar /usr/local/objective8
+mv migrations /usr/local/objective8
+mv objective8_config /etc/default
+sudo mv objective8d /etc/init.d
 sudo service objective8d start
