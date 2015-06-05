1ea27a977bedf112a2fb418c821937589c5d03e1
Duncan Mortimer <dmortime@thoughtworks.com>
2015-06-05 13:59:47 +0100

    CW + DM #139 - Serve static files using nginx

--- init-script/remote_start_objective8.sh
+++ init-script/remote_start_objective8.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# Halt running instance
 [ -f /etc/init.d/objective8d ] && sudo service objective8d stop || true
+
+# Set up static resources
+mkdir -p /var/www/objective8
+cp -r public /var/www/objective8
+
+# Launch application
 mkdir -p /usr/local/objective8
 cp objective8-0.0.1-SNAPSHOT-standalone.jar /usr/local/objective8
 cp -r migrations /usr/local/objective8
