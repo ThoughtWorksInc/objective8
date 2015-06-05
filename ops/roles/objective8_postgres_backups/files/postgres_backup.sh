a777202ea176fa632dd282db9b199664c4e7de28
Rob Speller <rspeller@thoughtworks.com>
2015-06-05 13:09:34 +0100

    RS + AB | #144 | Automate backups and send to s3

--- ops/roles/objective8_postgres_backups/files/postgres_backup.sh
+++ ops/roles/objective8_postgres_backups/files/postgres_backup.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
 
-mkdir -p /usr/local/objective8/backups
-pg_dump -U objective8 -h localhost > /usr/local/objective8/backups/backup_$(date +"%Y%m%d%H%M").bak
+dir=/usr/local/objective8
+cd $dir
+mkdir -p $dir/backups
+backup_path=$dir/backups/backup_$(date +"%Y%m%d%H%M").bak
+PGPASSFILE=$dir/.pgpass pg_dump -U objective8 -h localhost > $backup_path
+
+bucket_name=objective8_pg_db_backups
+s3cmd mb s3://$bucket_name -c $dir/.s3cfg
+s3cmd put $backup_path s3://$bucket_name -c $dir/.s3cfg
