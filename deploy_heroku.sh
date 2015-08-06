c8b714565d64e90da40ddac0c19df9dbc863112a
Rob Speller <rspeller@thoughtworks.com>
2015-08-06 10:35:29 +0100

    RS & AB | 128 | Add heroku deploy script for multiple buildpacks

---
+++ deploy_heroku.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+heroku git:remote --app objective8
+heroku maintenance:on
+heroku buildpacks:clear
+heroku buildpacks:set https://github.com/heroku/heroku-buildpack-clojure
+heroku buildpacks:add --index 1 https://github.com/heroku/heroku-buildpack-nodejs
+git push heroku master
+heroku maintenance:off
+
