b487f41ebd894ca05c26059eefc0250cc8867add
Christoph Wong <chwong@thoughtworks.com>
2015-09-25 12:15:42 +0100

    CW | 212 | udpated deploy script and ansible script to use docker config

---
+++ deploy_prod.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+npm install
+lein do clean, uberjar
+DIR=deploy_assets
+TAR=deploy.tar.gz
+mkdir -p $DIR
+cp target/*-standalone.jar $DIR
+cp -r resources/public $DIR
+cp -r init-script $DIR
+cp -r migrations $DIR
+tar -cvzf $TAR $DIR
+
+#ssh $REMOTE_USER@$SERVER_IP "mkdir -p /var/objective8/config"
+#scp objective8_env $REMOTE_USER@$SERVER_IP:/var/objective8/config/objective8_config 
+scp $TAR $REMOTE_USER@$SERVER_IP:/var/objective8/$TAR
+ssh $REMOTE_USER@$SERVER_IP <<EOF
+   sudo docker stop objective8 || echo 'Failed to stop objective8 container'
+   sudo docker rm objective8 || echo 'Failed to remove objective8 container'
+   sudo docker run -d --name objective8 --env-file=/usr/local/objective8/objective8_config -p 8080:8080 -v /var/objective8/$TAR:/$TAR java:8 bash -c 'tar -xvzf $TAR; cd $DIR; bash init-script/remote_start_objective8_docker.sh'
+EOF
+rm -rf $DIR
+lein do clean
--- init-script/remote_start_objective8_docker.sh
+++ init-script/remote_start_objective8_docker.sh
@@ -5,6 +5,6 @@ cp -r public /var/www/objective8
 
 # Launch application
 mkdir -p /usr/local/objective8
-cp objective8-0.0.1-SNAPSHOT-standalone.jar /usr/local/objective8
+cp *-standalone.jar /usr/local/objective8
 cp -r migrations /usr/local/objective8
 java -Dlog4j.configuration=log4j.debug -server -Xms256M -Xmx512M -XX:MaxPermSize=128M -jar /usr/local/objective8/*standalone.jar
