ccca5a4ec40bcff399b145b90e8e828f59298d33
John Cowie <jcowie@thoughtworks.com>
2015-09-30 15:43:07 +0100

    JC | Change staging provision script to point to staging docker config file

--- ops/provision-objective8-staging.sh
+++ ops/provision-objective8-staging.sh
@@ -1,1 +1,1 @@
-ansible-playbook digital_ocean_box_playbook.yml -i digital_ocean_box_staging.inventory --extra-vars="CONFIG_FILE_PATH=objective8_docker_config"
+ansible-playbook digital_ocean_box_playbook.yml -i digital_ocean_box_staging.inventory --extra-vars="CONFIG_FILE_PATH=objective8_staging_docker_config"
