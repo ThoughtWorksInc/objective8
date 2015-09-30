5538b0a1824a72f39623737736bf9061b70941a0
John Cowie <jcowie@thoughtworks.com>
2015-09-30 12:16:54 +0100

    JC | #213 | Added scripts for provisioning to prod and staging

---
+++ ops/provision-objective8-staging.sh
@@ -0,0 +1,1 @@
+ansible-playbook digital_ocean_box_playbook.yml -i digital_ocean_box_staging.inventory --extra-vars="CONFIG_FILE_PATH=objective8_docker_config"
---
+++ ops/provision-objective8.sh
@@ -0,0 +1,1 @@
+ansible-playbook digital_ocean_box_playbook.yml -i digital_ocean_box.inventory --extra-vars="CONFIG_FILE_PATH=objective8_docker_config"
