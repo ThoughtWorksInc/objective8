922d2f27ee8fe1e3176b0ebe5eade35e86e983d4
John Cowie <jcowie@thoughtworks.com>
2015-10-01 14:32:29 +0100

    JC | Update deploy_vagrant to match deploy_prod

--- deploy_vagrant.sh
+++ deploy_vagrant.sh
@@ -14,9 +14,12 @@ cp -r init-script $DIR
 cp -r migrations $DIR
 tar -cvzf $TAR $DIR
 ssh $REMOTE_USER@$SERVER_IP <<EOF
+   tar -xvfz /var/objective8/$TAR -C /var/objective8
+   mkdir -p /var/www/objective8
+   cp -r /var/objective8/$DIR/public /var/www/objective8
    sudo docker stop objective8 || echo 'Failed to stop objective8 container'
    sudo docker rm objective8 || echo 'Failed to remove objective8 container'
-   sudo docker run -d --name objective8 --env-file=/usr/local/objective8/objective8_config -p 8080:8080 -v /var/objective8/$TAR:/$TAR java:8 bash -c 'tar -xvzf $TAR; cd $DIR; bash init-script/remote_start_objective8_docker.sh'
+   sudo docker run -d --name objective8 --env-file=/usr/local/objective8/objective8_config -p 8080:8080 -p 8081:8081 -v /var/objective8/$TAR:/$TAR java:8 bash -c 'tar -xvzf $TAR; cd $DIR; bash init-script/remote_start_objective8_docker.sh'
 EOF
 rm -rf $DIR
 lein do clean
