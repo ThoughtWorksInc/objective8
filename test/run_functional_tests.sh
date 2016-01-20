42add248eca827ff54951cdf0c8651564e5b5556
Robert Bagge <rbagge@thoughtworks.com>
2016-01-20 16:35:59 +0000

    RB | quick fix | Changed readme to not include faulty repository source in git clone command

--- test/run_functional_tests.sh
+++ test/run_functional_tests.sh
@@ -1,8 +1,17 @@
-#!/usr/bin/env bash 
-start-stop-daemon --start -b -x /usr/bin/Xvfb -- :1 -screen 0 1280x1024x16
-JAVA_OPTS="-Dlog4j.configuration=log4j.test" \
-API_BEARER_NAME=functionalTests \
-API_BEARER_TOKEN=functionalTestsToken \
-GA_TRACKING_ID="" \
-DISPLAY=:1 lein midje objective8.functional.* $*
-start-stop-daemon --stop -x /usr/bin/Xvfb
+#!/usr/bin/env bash
+if [ -z "$DISPLAY" ]
+  then
+    start-stop-daemon --start -b -x /usr/bin/Xvfb -- :1 -screen 0 1280x1024x16
+    JAVA_OPTS="-Dlog4j.configuration=log4j.test" \
+    API_BEARER_NAME=functionalTests \
+    API_BEARER_TOKEN=functionalTestsToken \
+    GA_TRACKING_ID="" \
+    DISPLAY=:1 lein midje objective8.functional.* $*
+    start-stop-daemon --stop -x /usr/bin/Xvfb
+  else
+    JAVA_OPTS="-Dlog4j.configuration=log4j.test" \
+    API_BEARER_NAME=functionalTests \
+    API_BEARER_TOKEN=functionalTestsToken \
+    GA_TRACKING_ID="" \
+    DISPLAY=:1 lein midje objective8.functional.* $*
+fi
