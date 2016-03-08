18aaf78d447022cf00e4cecaa1f059524aefeb06
Seraphiana <seraphiana@hotmail.co.uk>
2016-03-08 17:24:31 +0000

    AW + FM | 309 | updated deploy script to use docker image

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,26 +1,8 @@
 #!/bin/bash
 
-npm install
-lein do clean, uberjar
-DIR=deploy_assets
-TAR=deploy.tar.gz
-mkdir -p $DIR
-cp target/*-standalone.jar $DIR
-cp -r resources/public $DIR
-cp -r init-script $DIR
-cp -r migrations $DIR
-tar -cvzf $TAR $DIR
-
-scp $TAR $REMOTE_USER@$SERVER_IP:/var/objective8
 ssh $REMOTE_USER@$SERVER_IP <<EOF
-   tar -xvzf /var/objective8/$TAR -C /var/objective8
-   mkdir -p /var/www/objective8
-   cp -r /var/objective8/$DIR/public /var/www/objective8  
    sudo docker stop objective8 || echo 'Failed to stop objective8 container'
    sudo docker rm objective8 || echo 'Failed to remove objective8 container'
-   sudo docker run -d --name objective8 --env-file=/usr/local/objective8/objective8_config -p 8080:8080 -p 8081:8081 -v /var/objective8/$TAR:/$TAR java:8 bash -c 'tar -xvzf /$TAR; cd /$DIR; bash init-script/remote_start_objective8_docker.sh'
-   rm /var/objective8/$TAR
-   rm -rf /var/objective8/$DIR
+   sudo docker rmi dcent/objective8 || echo 'Failed to remove objective8 image'
+   sudo docker run -d --env-file=/usr/local/objective8/objective8_config -p 8080:8080 -p 8081:8081 --name objective8 dcent/objective8
 EOF
-rm -rf $DIR
-lein do clean
