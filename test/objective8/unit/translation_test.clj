b7fb82aa4da96a7bfff587c111d04586f6155490
Seraphiana <seraphiana@hotmail.co.uk>
2016-03-10 15:28:06 +0000

    AW + FM | 341 | translation testing automatically includes added languages

--- test/objective8/unit/translation_test.clj
+++ test/objective8/unit/translation_test.clj
@@ -1,7 +1,8 @@
 (ns objective8.unit.translation-test
   (:require [midje.sweet :refer :all]
             [objective8.front-end.translation :as tr]
-            [clojure.algo.generic.functor :as algs]))
+            [clojure.algo.generic.functor :as algs]
+            [clojure.java.io :as io]))
 
 (def translations-directory "translations")
 
@@ -11,17 +12,19 @@
 (defn map-of-maps->map-of-sets [lang-map]
   (algs/fmap map->key-set lang-map))
 
-(tabular
-  (fact "All translation files contain the same entries"
-        (let [languages (tr/load-translations (tr/find-translation-resources translations-directory))
-              en-map (:en languages)
-              lang-map (?language languages)
-              valueless-en-map (map-of-maps->map-of-sets en-map)
-              valueless-lang-map (map-of-maps->map-of-sets lang-map)
-              difference (clojure.data/diff valueless-en-map valueless-lang-map)
-              ]
-          (first difference) => nil
-          ))
-  ?language
-  :es)
+(defn check-diff [map-primary map-secondary]
+  (first (clojure.data/diff map-primary map-secondary)))
+
+(defn test-languages [en-map other-languages-map]
+  (algs/fmap (partial check-diff en-map) other-languages-map))
+
+(facts "translations"
+      (let [languages (tr/load-translations (tr/find-translation-resources translations-directory))
+            en-map (:en languages)
+            other-languages (dissoc languages :en)
+            lang-seq (keys other-languages)
+            valueless-en-map (map-of-maps->map-of-sets en-map)
+            valueless-other-lang-map (algs/fmap map-of-maps->map-of-sets other-languages)
+            differences (test-languages valueless-en-map valueless-other-lang-map)]
+        differences => (has every? nil?)))
 
