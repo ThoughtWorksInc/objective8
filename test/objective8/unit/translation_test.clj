de9bb2e0e4e0d873bc6c763fe55b7a691fe37dd3
aewelch <amyelwelch@gmail.com>
2016-03-24 13:44:14 +0000

    AW | 0 | translation testing now tests for missing keys in the English translations

--- test/objective8/unit/translation_test.clj
+++ test/objective8/unit/translation_test.clj
@@ -1,8 +1,7 @@
 (ns objective8.unit.translation-test
   (:require [midje.sweet :refer :all]
             [objective8.front-end.translation :as tr]
-            [clojure.algo.generic.functor :as algs]
-            [clojure.java.io :as io]))
+            [clojure.algo.generic.functor :as algs]))
 
 (def translations-directory "translations")
 
@@ -13,18 +12,18 @@
   (algs/fmap map->key-set lang-map))
 
 (defn check-diff [map-primary map-secondary]
-  (first (clojure.data/diff map-primary map-secondary)))
+  (let [diff (clojure.data/diff map-primary map-secondary)]
+    (concat (first diff) (second diff))))
 
 (defn test-languages [en-map other-languages-map]
   (algs/fmap (partial check-diff en-map) other-languages-map))
 
-(facts "translations"
+(facts "the translations have the same keys in all languages"
       (let [languages (tr/load-translations (tr/find-translation-resources translations-directory))
             en-map (:en languages)
             other-languages (dissoc languages :en)
-            lang-seq (keys other-languages)
             valueless-en-map (map-of-maps->map-of-sets en-map)
             valueless-other-lang-map (algs/fmap map-of-maps->map-of-sets other-languages)
             differences (test-languages valueless-en-map valueless-other-lang-map)]
-        differences => (has every? nil?)))
+        differences => (has every? empty?)))
 
