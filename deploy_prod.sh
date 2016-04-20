6cc25813769c5be07bc99f435e86cccce7239684
aewelch <amyelwelch@gmail.com>
2016-04-20 10:31:37 +0100

    AW | 377 | deploy script can now be used with users other than root

--- deploy_prod.sh
+++ deploy_prod.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 ssh $REMOTE_USER@$SERVER_IP <<EOF
-   sudo docker stop objective8 || echo 'Failed to stop objective8 container'
+   echo $REMOTE_PASSWORD | sudo -S docker stop objective8 || echo 'Failed to stop objective8 container'
    sudo docker rm objective8 || echo 'Failed to remove objective8 container'
    sudo docker rmi dcent/objective8 || echo 'Failed to remove objective8 image'
    sudo docker run -d --env-file=/usr/local/objective8/objective8_config -p 8080:8080 -p 8081:8081 --name objective8 --restart=on-failure dcent/objective8
