6044c4460bb5f52c03720eb3cc88239d8a8e265e
Nicola Hughes <nihughes@thoughtworks.com>
2016-04-21 10:33:50 +0100

    NH | 0 | Reconfiguring start app vm to use localhost as VM now has forwarding port

--- start_app_vm.sh
+++ start_app_vm.sh
@@ -3,7 +3,7 @@
 grunt build
 API_BEARER_NAME=abc \
 API_BEARER_TOKEN=def \
-BASE_URI=192.168.50.50:8080 \
+BASE_URI=localhost:8080 \
 TWITTER_CONSUMER_TOKEN="stub-token" \
 TWITTER_CONSUMER_SECRET_TOKEN="stub-secret" \
 lein repl
